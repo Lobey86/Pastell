91a27c6950b940c90c07927d17ca029299a4cc83
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2011-03-15 10:38:37 +0000

    reprise correction précédente

--- web/utilisateur/modif-password.php
+++ web/utilisateur/modif-password.php
@@ -13,7 +13,7 @@ $lienRetourHTML->display("Espace utilisateur","utilisateur/moi.php");
 <div class="box_contenu clearfix">
 
 <h2>Modifier votre mot de passe</h2>
-<form  action='utilisateur/modif-password-controler.php' method='post' >
+<form class="w700"  action='utilisateur/modif-password-controler.php' method='post' >
 <table >
 
 <tr>
