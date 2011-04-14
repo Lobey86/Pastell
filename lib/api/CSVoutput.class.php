b901c425be0132c70ef4763fdef3ac4090db1ddb
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2011-04-14 08:00:49 +0000

    Ajout d'un lien dans le journal vers l'export CSV

--- lib/api/CSVoutput.class.php
+++ lib/api/CSVoutput.class.php
@@ -4,7 +4,7 @@
 class CSVoutput {
 	
 	public function sendAttachment($file_name,array $info){
-		header("Content-type: text/csv");
+		header("Content-type: text/csv; charset=iso-8859-1");
 		header("Content-disposition: attachment; filename=$file_name");
 		header("Expires: 0");
 		header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
