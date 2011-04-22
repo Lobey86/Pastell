ea31c1635c836f844a329f21bedba38d576abb7c
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2011-04-22 08:45:10 +0000

    Evolution #5009 Ajout de la liste de tous les utilisateurs et moteur de recherche dessus

--- lib/utilisateur/UtilisateurListeHTML.class.php
+++ lib/utilisateur/UtilisateurListeHTML.class.php
@@ -8,6 +8,36 @@ class UtilisateurListeHTML {
 		$this->droitEdition = true;
 	}
 	
+	public function displayAll(array $liste_utilisateur){
+		?>
+	
+		
+		<table class='tab_02'>
+		<tr>
+			<th>Prénom Nom</th>
+			<th>login</th>
+			<th>email</th>
+			<th>entité de base</th>					
+		</tr>
+		
+		<?php foreach($liste_utilisateur as $user) : ?>
+			<tr>
+				<td>
+					<a href='utilisateur/detail.php?id_u=<?php echo $user['id_u'] ?>'>
+						<?php echo $user['prenom']?> <?php echo $user['nom']?>
+					</a>
+				</td>
+				<td><?php echo $user['login']?></td>
+				<td><?php echo $user['email']?></td>
+				<td><a href='entite/detail.php?id_e=<?php echo $user['id_e']?>'><?php echo $user['denomination']?></a></td>
+			</tr>
+		<?php endforeach; ?>
+		
+		</table>
+		
+		<?php
+	}
+	
 	public function display(array $liste_utilisateur,$id_e){
 		
 		?>
