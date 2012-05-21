f67e03ce5357997f0bba0fb1c04cc6c51a9afa38
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2012-05-21 07:25:34 +0000

    Correction message AccuserReception

--- trunk/action/AccuserReception.class.php
+++ trunk/action/AccuserReception.class.php
@@ -11,7 +11,12 @@ class AccuserReception extends ActionExecutor {
 		$this->getActionCreator()->addAction($this->id_e,$this->id_u,$this->action, "Vous avez accusé réception de ce message");
 		$this->getActionCreator()->addToEntite($id_ged,"Un accusé de réception a été recu pour le document");
 		
-		$message = "Un accusé de réception a été recu pour le document  " . $this->id_d;
+		$infoDocument = $this->getDocument()->getInfo($this->id_d);
+		
+		$message = "Un accusé de réception a été recu pour le document  {$infoDocument['titre']}"; 
+		$message .= "\n\nConsulter le détail du document : " . SITE_BASE . "document/detail.php?id_d={$this->id_d}&id_e={$this->id_e}";
+	
+		
 		$this->getNotificationMail()->notify($id_ged,$this->id_d,$this->action, $this->type,$message);
 
 
