9fd49ce532b918a2df15c47fa6377223ef66940d
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2012-12-12 09:37:57 +0000

    Supression external-data

--- trunk/externaldata/fichier-nomemclature-controler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$fieldValue = $recuperateur->get($field);
-
-$donneesFormulaire = $objectInstancier->ConnecteurFactory->getConnecteurConfig($id_ce);
-$donneesFormulaire->setData($field,$fieldValue);
-
-header("Location: edition-modif.php?id_ce=$id_ce");
-
