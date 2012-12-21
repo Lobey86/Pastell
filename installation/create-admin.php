40563e1531689b97b3f0172e4dd9f05e2dd32565
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2012-12-21 08:56:13 +0000

    Bugfixe : création du rôle « admin » à l'installation de Pastell

--- installation/create-admin.php
+++ installation/create-admin.php
@@ -3,17 +3,16 @@
 
 require_once( dirname(__FILE__) . "/../web/init.php");
 
-function getArgv($nb){
-	global $argv;
-	if (isset($argv[$nb])){
-		return $argv[$nb];
+$get_argv = function ($num_arg) use ($argv){
+	if (empty($argv[$num_arg])){
+		return false;
 	}
-	return false;
-}
+	return $argv[$num_arg];
+};
 
-$login = getArgv(1);
-$password = getArgv(2);
-$email = getArgv(3);
+$login = $get_argv(1);
+$password = $get_argv(2);
+$email = $get_argv(3);
 
 $result = $objectInstancier->AdminControler->createAdmin($login,$password,$email);
 
@@ -23,4 +22,7 @@ if ($result){
 } else {
 	echo $objectInstancier->AdminControler->getLastError()."\n";	
 	echo "Usage : {$argv[0]} login password email\n";
+	exit;
 }
+
+$objectInstancier->AdminControler->fixDroit();
