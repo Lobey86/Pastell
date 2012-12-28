88cb30688c68a73eed8b62d9a16d5f8deab53e7f
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2012-12-28 10:24:04 +0000

    Ajout de l'export du journal via des filtres

---
+++ trunk/controler/JournalControler.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class JournalControler extends PastellControler {
+	
+	public function exportAction(){
+				
+		$recuperateur = new Recuperateur($_REQUEST);
+		$this->id_e = $recuperateur->getInt('id_e',0);
+		$this->type = $recuperateur->get('type');
+		$this->id_d = $recuperateur->get('id_d');
+		$this->id_u = $recuperateur->get('id_u');
+		
+		
+		$this->entite_info = $this->EntiteSQL->getInfo($this->id_e);	
+		$this->utilisateur_info = $this->Utilisateur->getInfo($this->id_u);
+		$this->document_info = $this->Document->getInfo($this->id_d);
+		
+		
+		$this->recherche = $recuperateur->get('recherche');
+		$this->date_debut = $recuperateur->get('date_debut',date("Y-m-d"));
+		$this->date_fin = $recuperateur->get('date_fin',date("Y-m-d"));
+	
+		$this->ensureDroit('journal:lecture', $this->id_e);
+		
+		$this->page_title="Journal des évènements - Export";
+		$this->template_milieu = "JournalExport";
+		$this->renderDefault();
+		
+	}
+	
+	
+}
--- trunk/lib/helper/date.php
+++ trunk/lib/helper/date.php
@@ -8,3 +8,7 @@ function date_iso_to_fr($date){
 function time_iso_to_fr($datetime){
 	return date("d/m/Y H:i:s",strtotime($datetime));
 }
+
+function date_fr_to_iso($date){
+	return preg_replace("#^(\d{2})/(\d{2})/(\d{4})$#",'$3-$2-$1',$date);	
+}
