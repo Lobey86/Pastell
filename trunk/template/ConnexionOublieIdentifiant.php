73cebf98e7c09796a074187e17d3ad829e59ff2f
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2012-12-29 08:12:57 +0000

    Extraction dans template pour la page ConnexionOublieIdentifiant (#5882)

---
+++ trunk/template/ConnexionOublieIdentifiant.php
@@ -0,0 +1,37 @@
+
+<div class="w500">
+
+<div class="box_contenu clearfix">
+	<div class="box_connexion">
+		<h2>Merci d'indiquer une information</h2>
+		
+		<div class='box_info'>
+		Afin que nous puissions permettre la réinitialisation du mot de passe, 
+		veuillez indiquer l'une des deux informations suivantes :
+		</div>
+		
+		<form action='connexion/oublie-identifiant-controler.php' method='post' >
+		<table>
+		<tr>
+		<th class="w50pc"><label for="login">Votre identifiant</label></th>
+		<td class="w50pc"><input type="text" name="login" id="login" class='noautocomplete'/></td>
+		</tr>
+			<tr>
+		<th class="w50pc"><label for="email"><b>OU</b> Votre email</label></th>
+		<td class="w50pc"><input type="text" name="email" id="email" class='noautocomplete'/></td>
+		</tr>
+		</table>
+		
+		<div class="float_left">
+		<a href="connexion/connexion.php">Retourner à la connexion</a>
+		</div>
+		
+		<div class="align_right">
+		<input type="submit" value="Envoyer" class="submit" />
+		</div>
+		
+		</form>
+	
+	</div>
+</div>
+</div>
