a36b37ddb9d1f02cd9f55af052bd829e9096ec62
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-01-18 06:35:26 +0000

    Ajout du titre dans le message du journal lors de la supression du document

--- trunk/action/Supprimer.class.php
+++ trunk/action/Supprimer.class.php
@@ -3,10 +3,12 @@
 class Supprimer extends ActionExecutor {
 
 	public function go(){
+		$info = $this->getDocument()->getInfo($this->id_d);
+		
 		$this->getDonneesFormulaire()->delete();
 		$this->getDocument()->delete($this->id_d);
 		
-		$message = "Le document {$this->id_d} à été supprimé";
+		$message = "Le document « {$info['titre']} » ({$this->id_d}) à été supprimé";
 		$this->getJournal()->add(Journal::DOCUMENT_ACTION,$this->id_e,$this->id_d,"supression",$message);
 		
 		$this->setLastMessage($message);
