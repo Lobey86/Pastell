d5355e927b618790cf5cbbac68f61727f2d74573
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-02-19 08:08:29 +0000

    Ajout du traitement de la transaction d'annulation

--- module/actes/action/TedetisAnnulation.class.php
+++ module/actes/action/TedetisAnnulation.class.php
@@ -7,11 +7,14 @@ class TedetisAnnulation  extends ActionExecutor {
 		$tdT = $this->getConnecteur("TdT"); 
 		$tedetis_transaction_id = $this->getDonneesFormulaire()->get('tedetis_transaction_id');
 		
-		
-		if (!  $tdT->annulationActes($tedetis_transaction_id) ){
+		$id_annulation_transaction = $tdT->annulationActes($tedetis_transaction_id); 
+		if (!  $id_annulation_transaction ){
 			$this->setLastMessage( $tedetis->getLastError());
 			return false;
 		}
+		$this->getDonneesFormulaire()->setData('tedetis_annulation_id',$id_annulation_transaction);
+		$this->getDonneesFormulaire()->setData('has_annulation',true);
+		
 		$message  = "Une notification d'annulation a été envoyé au contrôle de légalité";
 		$this->getActionCreator()->addAction($this->id_e,$this->id_u,$this->action,$message);
 			
