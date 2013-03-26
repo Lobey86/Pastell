bbe845243e4c5dd644d5af800a24eee55cb70565
pviver <pviver@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-03-26 11:10:32 +0000

    bug f#6454 : correction fautes d'orthographe

--- template/DocumentAR.php
+++ template/DocumentAR.php
@@ -1,7 +1,7 @@
 
 <div>
 
-<h2>L'expediteur a demandé un accusé de réception</h2>
+<h2>L'expéditeur a demandé un accusé de réception</h2>
  
 <form action='document/action.php' method='post'>
 	<input type='hidden' name='id_d' value='<?php echo $id_d ?>' />
