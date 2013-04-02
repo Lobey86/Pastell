f6756a44237bf9174dc6f13450d656918bf56209
pviver <pviver@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-04-02 15:21:00 +0000

    bug f#6454 : correction fautes d'orthographe

--- template/UtilisateurModifEmailConfirm.php
+++ template/UtilisateurModifEmailConfirm.php
@@ -9,13 +9,13 @@
 	<p>Votre email a été validé.</p>
 	
 	
-	<p>Votre administrateur doit maintenant valider votre changement d'email</p>
-	<p>Vous serez averti par email </p>
+	<p>Votre administrateur doit maintenant valider votre changement d'email.</p>
+	<p>Vous serez averti par email.</p>
 </div>	
 <?php else : ?>
 <div class="box_error">
 
-<p>Un problème empêche de satisfaire votre demande</p>
+<p>Un problème empêche de satisfaire votre demande.</p>
 <p>Veuillez recommencer la procédure de changement d'email.</p>
 </div>	
 <?php endif;?>
