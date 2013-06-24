6f4b6ff37ea8b587a4539164977fae6bc043f2c4
labylo <labylo@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-06-24 16:59:23 +0000

    Commit non fonctionnel (Mise à jour de BootStrap)

--- template-bs/EntiteNavigation.php
+++ template-bs/EntiteNavigation.php
@@ -1,27 +1,3 @@
-<div class="box" style="border:1px solid red;">
-	<h2>Navigation dans les collectivités</h2>
-	<?php if ($navigation_entite_affiche_toutes) : ?>
-		<a href='<?php echo $navigation_url?>'>Toutes</a>
-	<?php endif;?>
-	<?php foreach($navigation_all_ancetre as $ancetre) : ?>
-		&gt; <a href='<?php echo $navigation_url ?>&id_e=<?php echo $ancetre['id_e']?>'><?php echo $ancetre['denomination']?></a>
-	<?php endforeach; ?>
-	<?php if ($navigation_denomination) : ?>
-		&gt; <b><?php echo $navigation_denomination ?></b>
-	<?php endif;?>
-	<ul>
-	<?php foreach($navigation_liste_fille as $fille) : ?>
-		<li>
-			<a href='<?php echo $navigation_url ?>&id_e=<?php echo $fille['id_e'] ?>'>
-				<?php hecho($fille['denomination']) ?>
-			</a>
-		</li>
-	<?php endforeach;?>
-	</ul>
-</div>
-
-
-
 <div class="box">
 	<h2>Navigation dans les collectivités</h2>
 	
