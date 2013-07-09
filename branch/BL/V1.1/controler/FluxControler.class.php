d2fccf35f4f7c463fdd6d4851628e38c910b171e
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-07-09 07:50:44 +0000

    - API Provisioning avec les modifications du core Pastell nécessaires - Suppression d'un controle sur l'existance du flux si aucun flux n'est passé en paramètre : Cas des connecteurs globaux et flux 'global'

--- branch/BL/V1.1/controler/FluxControler.class.php
+++ branch/BL/V1.1/controler/FluxControler.class.php
@@ -13,14 +13,22 @@ class FluxControler extends PastellControler {
 		return $connecteur_disponible;
 	}
 	
+        // Refactoring : exception au lieu de la redirection
 	public function hasGoodType($id_ce,$type){
 		$info = $this->ConnecteurEntiteSQL->getInfo($id_ce);
 		if ($info['type'] != $type){
-			$this->LastError->setLastError("Le connecteur n'est pas du bon type");
-			$this->redirect("/entite/detail.php?id_e=$id_e&page=3");
+                    throw new Exception("Le connecteur n'est pas du bon type.");
+//			$this->LastError->setLastError("Le connecteur n'est pas du bon type");
+//			$this->redirect("/entite/detail.php?id_e=$id_e&page=3");
 		}
 	}
-	
+        
+	// Refactoring - Nouvelle méthode        
+        public function redirectWhenError($id_e) {
+            $this->redirect("/entite/detail.php?id_e=$id_e&page=3");
+        }
+        
+        // Refactoring
 	public function doEditionModif(){
 		$recuperateur = new Recuperateur($_POST);
 		$id_e = $recuperateur->getInt('id_e');
@@ -29,14 +37,32 @@ class FluxControler extends PastellControler {
 		$id_ce = $recuperateur->get('id_ce');
 
 		$this->hasDroitEdition($id_e);
-		$this->hasGoodType($id_ce, $type);
-		
-		$this->FluxEntiteSQL->addConnecteur($id_e,$flux,$type,$id_ce);
-		
-		$this->LastMessage->setLastMessage("Connecteur associé au flux avec succès");
-		$this->redirect("/entite/detail.php?id_e=$id_e&page=4");
-	}
+		try {
+                    $this->hasGoodType($id_ce, $type);
+                    $this->editionModif($id_e, $flux, $type, $id_ce);
+                    $this->LastMessage->setLastMessage("Connecteur associé au flux avec succès");
+                    $this->redirect("/entite/detail.php?id_e=$id_e&page=4");
+                } catch (Exception $ex) {
+                    $this->LastError->setLastError($ex->getMessage());
+                    $this->redirectWhenError($id_e);
+                }                                
+	}                
 	
+        // Nouvelle methode commune IHM et API
+        public function editionModif($id_e, $flux, $type, $id_ce) {
+            
+            $this->hasGoodType($id_ce, $type);
+            if ($flux!=null) {
+                $info = $this->FluxDefinitionFiles->getInfo($flux);
+                if (!$info) {
+                    throw new Exception("Le type de flux n'existe pas.");
+                }              
+            }
+            $id_fe = $this->FluxEntiteSQL->addConnecteur($id_e,$flux,$type,$id_ce);
+            return $id_fe;
+        }
+
+        
 	public function doSupprimer(){
 		$recuperateur = new Recuperateur($_POST);
 		$id_e = $recuperateur->getInt('id_e');
