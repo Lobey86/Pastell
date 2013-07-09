4c70814e14769a6a3d9ab3117bb53210d06abac5
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-07-09 07:47:52 +0000

    - API Provisioning avec les modifications du core Pastell nécessaires

--- branch/BL/V1.1/controler/AdminControler.class.php
+++ branch/BL/V1.1/controler/AdminControler.class.php
@@ -9,7 +9,8 @@ class AdminControler extends Controler {
 			$this->lastError = $this->UtilisateurCreator->getLastError();
 			return false; 
 		}
-		
+                //Ajout de l'affectation du nom (reprise du login) pour avoir accès à la fiche de l'utilisateur depuis l'IHM
+		$this->Utilisateur->setNomPrenom($id_u,$login,"");             
 		$this->Utilisateur->validMailAuto($id_u);
 		$this->Utilisateur->setColBase($id_u,0);
 		$this->RoleUtilisateur->addRole($id_u,"admin",0);
