09d4ddd07222783218f1ae380876aa8504571468
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-07-09 08:02:11 +0000

    - Ajout de paramètres aux actions : Ces actions ne sont utilisables seulement par API. Par l'ihm, on ne peut pas passer les paramètres aux actions.

--- branch/BL/V1.1/web/api/action.php
+++ branch/BL/V1.1/web/api/action.php
@@ -7,7 +7,11 @@ $id_d = $recuperateur->get('id_d');
 $action = $recuperateur->get('action');
 $id_destinataire = $recuperateur->get('destinataire');
 if (!$id_destinataire){
-	$id_destinataire = array();
+    $id_destinataire = array();
+}
+$action_params = $recuperateur->get('action_params');
+if (!$action_params) {
+    $action_params=array();
 }
 
-$api_json->action($id_e, $id_d,$action,$id_destinataire);
+$api_json->action($id_e, $id_d,$action,$id_destinataire, true, $action_params);
