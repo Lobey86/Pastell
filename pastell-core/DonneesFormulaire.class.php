ad629197d8fdec8e1340043d1bdb64b62a0a9ad3
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-09-27 05:23:36 +0000

    Prise en compte des fichiers OpenXML afin de leur donner un mime type plus correcte

--- pastell-core/DonneesFormulaire.class.php
+++ pastell-core/DonneesFormulaire.class.php
@@ -283,13 +283,40 @@ class DonneesFormulaire {
 		return file_get_contents($file_path);
 	}
 	
+	//http://stackoverflow.com/questions/6595183/docx-file-type-in-php-finfo-file-is-application-zip
+	private function getOpenXMLMimeType($file_name){
+		$ext = pathinfo($file_name,PATHINFO_EXTENSION);
+		$openXMLExtension = array(
+				'xlsx' => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+				'xltx' => "application/vnd.openxmlformats-officedocument.spreadsheetml.template",
+				'potx' =>  "application/vnd.openxmlformats-officedocument.presentationml.template",
+				'ppsx' =>  "application/vnd.openxmlformats-officedocument.presentationml.slideshow",
+				'pptx'   =>  "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+				'sldx'   =>  "application/vnd.openxmlformats-officedocument.presentationml.slide",
+				'docx'   =>  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+				'dotx'   =>  "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+				'xlam'   =>  "application/vnd.ms-excel.addin.macroEnabled.12",
+				'xlsb'   =>  "application/vnd.ms-excel.sheet.binary.macroEnabled.12");
+		if (isset($openXMLExtension[$ext])){
+			return $openXMLExtension[$ext];
+		}
+		return false;
+	}
+	
 	public function getContentType($field_name,$num  = 0){
 		$file_path = $this->getFilePath($field_name,$num);
 		if (! file_exists($file_path)){
 			return;
 		}
 		$fileInfo = new finfo();
-		return $fileInfo->file($file_path,FILEINFO_MIME_TYPE);
+		$result = $fileInfo->file($file_path,FILEINFO_MIME_TYPE);
+		
+		if ($result == 'application/zip'){
+			$file_name = $this->getFileName($field_name,$num);
+			$result = $this->getOpenXMLMimeType($file_name)?:'application/zip';
+		}
+		
+		return $result;
 	}
 	
 	public function getFileName($field_name,$num = 0){
