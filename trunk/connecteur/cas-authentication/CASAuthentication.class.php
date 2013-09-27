ae46c9e4f8554579f6d6af60bd6282de78d9fba5
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-09-27 09:33:37 +0000

    [Connecteur CAS] Ajout des champs cas_proxy et cas_debug

--- trunk/connecteur/cas-authentication/CASAuthentication.class.php
+++ trunk/connecteur/cas-authentication/CASAuthentication.class.php
@@ -6,18 +6,31 @@ class CASAuthentication extends Connecteur {
 	private $host;
 	private $port;
 	private $context;
+	private $ca_file;
+	private $proxy;
 	
 	public function setConnecteurConfig(DonneesFormulaire $donneesFormulaire){
 		$this->host = $donneesFormulaire->get('cas_host');
 		$this->port = $donneesFormulaire->get('cas_port');
 		$this->context = $donneesFormulaire->get('cas_context');
 		$this->ca_file = $donneesFormulaire->getFilePath('cas_ca');
-		phpCAS::setDebug("/tmp/cas.log");
+		$this->proxy = $donneesFormulaire->getFilePath('cas_proxy');
+		$cas_debug = $donneesFormulaire->getFilePath('cas_debug');
+		if ($cas_debug) {
+			phpCAS::setDebug($cas_debug);
+		}
+	}
+	
+	private function setClient(){
+		phpCAS::client(CAS_VERSION_2_0, $this->host,$this->port,$this->context);
+		phpCAS::setCasServerCACert($this->ca_file);
+		if ($this->proxy){
+			phpCAS::allowProxyChain(new CAS_ProxyChain(array($this->proxy)));
+		}
 	}
 	
 	public function isSessionAuthenticated(){
-		phpCAS::client(CAS_VERSION_2_0, $this->host,$this->port,$this->context);
-		phpCAS::setCasServerCACert($this->ca_file);
+		$this->setClient();
 		if (phpCAS::isSessionAuthenticated()){
 			phpCAS::handleLogoutRequests(false);
 			phpCAS::forceAuthentication();
@@ -27,8 +40,7 @@ class CASAuthentication extends Connecteur {
 	}
 	
 	public function authenticate($url){
-		phpCAS::client(CAS_VERSION_2_0, $this->host,$this->port,$this->context);
-		phpCAS::setCasServerCACert($this->ca_file);
+		$this->setClient();
 		phpCAS::setFixedServiceURL($url);
 		phpCAS::handleLogoutRequests(false);
 		phpCAS::forceAuthentication();
@@ -36,8 +48,7 @@ class CASAuthentication extends Connecteur {
 	}
 	
 	public function logout(){
-		phpCAS::client(CAS_VERSION_2_0, $this->host,$this->port,$this->context);
-		phpCAS::setCasServerCACert($this->ca_file);
+		$this->setClient();
 		return phpCAS::logout();
 	}
 	
