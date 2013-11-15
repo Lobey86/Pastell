7cbed4a88483002d4a38376105aa234d771d29ff
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-11-15 08:48:29 +0000

    Ajout de script de test pour PHPUnit

---
+++ test/PHPUnit/PasswordGeneratorTest.php
@@ -0,0 +1,25 @@
+<?php
+require_once(__DIR__."/../../lib/PasswordGenerator.class.php");
+
+class PasswordGeneratorTest extends PHPUnit_Framework_TestCase {
+
+	public function testGetPassword(){
+		$passwordGenerator = new PasswordGenerator();
+		$password = $passwordGenerator->getPassword();
+		$this->assertEquals(PasswordGenerator::NB_SIGNE_DEFAULT,strlen($password));
+		$this->assertRegExp("/^[".PasswordGenerator::SIGNE."]*$/",$password);
+	}
+
+	public function testSetPasswordLength(){
+		$passwordGenerator = new PasswordGenerator();
+		$password = $passwordGenerator->getPassword();
+		$this->assertEquals(PasswordGenerator::NB_SIGNE_DEFAULT,strlen($password));
+	}
+
+	public function testSetSignePassword(){
+		$passwordGenerator = new PasswordGenerator();
+		$passwordGenerator->setSigne("X");
+		$password = $passwordGenerator->getPassword();
+		$this->assertEquals("XXXXXXX",$password);
+	}
+}
---
+++ test/PHPUnit/SirenTest.php
@@ -0,0 +1,28 @@
+<?php 
+require_once(__DIR__.'/../../lib/Siren.class.php');
+
+class SirenTest extends PHPUnit_Framework_TestCase {
+
+	private $siren;
+	
+	public function setUp(){
+		$this->siren = new Siren();	
+	}
+	
+	public function testGood(){
+		$this->assertTrue($this->siren->isValid("493587273"));		
+	}
+	
+	public function testBadLength(){
+		$this->assertFalse($this->siren->isValid(""));		
+	}
+	
+	public function testBad(){
+		$this->assertFalse($this->siren->isValid("493587274"));		
+	}
+	
+	public function testGenerate(){
+		$this->assertTrue($this->siren->isValid($this->siren->generate()));
+	}
+	
+}
