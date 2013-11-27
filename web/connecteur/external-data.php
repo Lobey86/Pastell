d9c28e34b95f71356dc264b06ea58f70f65ca10b
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-11-27 07:38:26 +0000

    Prise en compte des exeptions dans l'affichage du display des liste de choix pour un connecteur

--- web/connecteur/external-data.php
+++ web/connecteur/external-data.php
@@ -10,7 +10,7 @@ $id_e  = $connecteur_info['id_e'];
 
 if ( ! $roleUtilisateur->hasDroit($authentification->getId(),"entite:edition",$id_e)) {
 	$lastError->setLastError("Vous n'avez pas le droit de faire cette action (entite:edition)");
-	header("Location: edition-properties.php?id_e=$id_e&page=$page");
+	header("Location: edition-modif.php?id_ce=$id_ce");
 	exit;
 }
 
@@ -19,7 +19,13 @@ $formulaire = $documentType->getFormulaire();
 
 $action_name =  $formulaire->getField($field)->getProperties('choice-action');
 if ($action_name) {
-	$result = $objectInstancier->ActionExecutorFactory->displayChoiceOnConnecteur($id_ce,$authentification->getId(),$action_name,$field);	
+	$result = $objectInstancier->ActionExecutorFactory->displayChoiceOnConnecteur($id_ce,$authentification->getId(),$action_name,$field);
+	if (! $result){
+		$lastError->setLastError($objectInstancier->ActionExecutorFactory->getLastMessage());
+		header("Location: edition-modif.php?id_ce=$id_ce");
+		exit;
+		
+	}	
 } else {
 	$script =  $formulaire->getField($field)->getProperties('script');
 	require_once(PASTELL_PATH . "/externaldata/$script");
