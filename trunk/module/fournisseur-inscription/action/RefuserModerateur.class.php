f22c55981515380880cec08942f3247e9bb93227
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-12-29 08:10:56 +0000

    Finalisation du flux inscription fournisseur avec ajout d'un modérateur Adullact

--- trunk/module/fournisseur-inscription/action/RefuserModerateur.class.php
+++ trunk/module/fournisseur-inscription/action/RefuserModerateur.class.php
@@ -1,25 +1,26 @@
 <?php
 
-class RefuserFournisseur extends ActionExecutor {
+class RefuserModerateur extends ActionExecutor {
 
 	public function go(){
 		$documentEntite = new DocumentEntite($this->getSQLQuery());
 		$id_fournisseur = $documentEntite->getEntiteWithRole($this->id_d,"editeur");
-	
+
 		$entite = new Entite($this->getSQLQuery(),$id_fournisseur);
 		$infoEntite = $entite->getInfo();
 		$nomFournisseur = $infoEntite['denomination'];
-		
+
 		$entite = new Entite($this->getSQLQuery(),$this->id_e);
 		$infoEntite = $entite->getInfo();
 		$nomCol = $infoEntite['denomination'];
-		
+
 		$actionCreator = $this->getActionCreator();
 		$actionCreator->addAction($this->id_e,$this->id_u,$this->action,"L'inscription de $nomFournisseur a été refusé");
 		$actionCreator->addToEntite($id_fournisseur,"$nomCol a refusé l'inscription");
+
 		$message = "Le fournisseur $nomFournisseur a été refusé par la collectivité $nomCol";
 		$this->setLastMessage($message);
 		$this->notify($this->action, $this->type, $message);
-		return true;				
+		return true;
 	}
 }
