8a7a778ff495147b72516c0d802f6c2946e383d6
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2013-12-29 08:10:56 +0000

    Finalisation du flux inscription fournisseur avec ajout d'un modérateur Adullact

--- model/CollectiviteFournisseurSQL.class.php
+++ model/CollectiviteFournisseurSQL.class.php
@@ -2,8 +2,7 @@
 class CollectiviteFournisseurSQL extends SQL {
 	
 	public function add($id_e_col,$id_e_fournisseur){
-		$sql = "SELECT count(*) FROM collectivite_fournisseur WHERE id_e_col = ? AND id_e_fournisseur=?";
-		if ($this->queryOne($sql,$id_e_col,$id_e_fournisseur)){
+		if ($this->getInfo($id_e_col, $id_e_fournisseur)){
 			return;
 		}
 		$sql = "INSERT INTO collectivite_fournisseur(id_e_col,id_e_fournisseur) VALUES (?,?)";
@@ -15,6 +14,16 @@ class CollectiviteFournisseurSQL extends SQL {
 				" JOIN entite ON collectivite_fournisseur.id_e_col=entite.id_e ". 
 				" WHERE id_e_fournisseur=?";
 		return $this->query($sql,$id_e_fournisseur);
+	}
+	
+	public function getInfo($id_e_col,$id_e_fournisseur){
+		$sql = "SELECT * FROM collectivite_fournisseur WHERE id_e_col = ? AND id_e_fournisseur=?";
+		return $this->queryOne($sql,$id_e_col,$id_e_fournisseur);
+	}
+	
+	public function validRelation($id_e_col,$id_e_fournisseur){
+		$sql = "UPDATE collectivite_fournisseur SET is_valid=1 WHERE id_e_col = ? AND id_e_fournisseur=?";
+		$this->query($sql,$id_e_col,$id_e_fournisseur);
 	}
 	
 }
--- module/fournisseur-inscription/action/AccepterFournisseur.class.php
+++ module/fournisseur-inscription/action/AccepterFournisseur.class.php
@@ -14,11 +14,37 @@ class AccepterFournisseur extends ActionExecutor {
 		$infoEntite = $entite->getInfo();
 		$nomCol = $infoEntite['denomination'];
 		
+		$id_moderateur = $this->getIdModerateur();
+		$this->getDocumentEntite()->addRole($this->id_d,$id_moderateur,"moderateur");
+		
+		
 		$actionCreator = $this->getActionCreator();
 		$actionCreator->addAction($this->id_e,$this->id_u,$this->action,"L'inscription de $nomFournisseur a été accepté");
 		$actionCreator->addToEntite($id_fournisseur,"$nomCol a accepté l'inscription");
+		$actionCreator->addToEntite($id_moderateur,"$nomCol a accepté l'inscription de $nomFournisseur");
 		
-		$this->setLastMessage("Le fournisseur a été accepté.");
+		$action = "envoi-moderateur";
+		$message = "Envoi de la demande à la modération";
+		$actionCreator = $this->getActionCreator();
+		$actionCreator->addAction($id_moderateur,0,$action,$message);
+		$actionCreator->addToEntite($id_fournisseur,$message);
+		$actionCreator->addToEntite($this->id_e,$message);
+		
+		$this->setLastMessage("La demande a été envoyé à la modération.");
+		
 		return true;			
 	}
+	
+	private function getIdModerateur(){
+		$validationFournisseur = $this->getGlobalConnecteur('validation-fournisseur');
+		if (! $validationFournisseur) {
+			throw new Exception("Aucun connecteur validation-fournisseur n'a été trouvé");
+		}
+		$id_e =  $validationFournisseur->getIdModerateur();
+		if (! $id_e) {
+			throw new Exception("Le connecteur validation-fournisseur n'est pas configuré correctement");
+		}
+		return $id_e;
+	}
+	
 }
