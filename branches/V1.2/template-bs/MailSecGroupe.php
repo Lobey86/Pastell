fbbcc41a6a093c588bb76e663d13cc0405d63d80
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-01-13 08:35:20 +0000

    Correction interface MailSecGroupe

--- branches/V1.2/template-bs/MailSecGroupe.php
+++ branches/V1.2/template-bs/MailSecGroupe.php
@@ -1,7 +1,7 @@
-<a href='mailsec/groupe-list.php?id_e=<?php echo $id_e ?>'>« Voir tous les groupes</a>
+<a class='btn btn-mini' href='mailsec/groupe-list.php?id_e=<?php echo $id_e ?>'><i class='icon-circle-arrow-left'></i> Voir tous les groupes</a>
 
 <br/><br/>
-<div class="box_contenu">
+<div class="box">
 <h2>Liste des contacts de «<?php echo $infoGroupe['nom']?>» </h2>
 
 <?php $this->SuivantPrecedent($offset,AnnuaireGroupe::NB_MAX,$nbUtilisateur,"mailsec/groupe.php?id_e=$id_e&id_g=$id_g"); ?>
@@ -12,7 +12,7 @@
 	<input type='hidden' name='id_e' value='<?php echo $id_e ?>' />
 	<input type='hidden' name='id_g' value='<?php echo $id_g ?>' />
 
-<table  class="tab_02">
+<table  class="table table-striped">
 	<tr>
 	
 		<th>Description</th>
@@ -28,7 +28,7 @@
 	
 </table>
 <?php if ($can_edit) : ?>
-<input type='submit' value='Enlever du groupe'/>
+<input type='submit' value='Enlever du groupe' class='btn'/>
 <?php endif; ?>
 
 </form>
@@ -36,27 +36,24 @@
 
 <?php if ( $roleUtilisateur->hasDroit($authentification->getId(),"annuaire:edition",$id_e)) : ?>
 
-<div class="box_contenu">
+<div class="box">
 <h2>Ajouter un contact à «<?php echo $infoGroupe['nom']?>» </h2>
 <form action='mailsec/add-contact-to-groupe.php' method='post' >		
 	<input type='hidden' name='id_e' value='<?php echo $id_e ?>' />
 	<input type='hidden' name='id_g' value='<?php echo $id_g ?>' />
 	
-	<table>
+	<table class="table table-striped">
 		<tbody>
 			<tr>
 				<th>Contact : </th>
-				<td><input type='text' id='name' name='name' value='' /></td>
+				<td><input type='text' id='nom_contact' name='name' value='' /></td>
 			</tr>	
 		</tbody>
 	</table>
 	<script>
-		 var format_item = function (item, position, length){ 
-		    return htmlentities("" + item,"ENT_QUOTES");
-		  } 
 	 
  		 $(document).ready(function(){
-				$("#name").autocomplete("mailsec/get-contact-ajax.php",  
+				$("#nom_contact").autocomplete("mailsec/get-contact-ajax.php",  
 						{
 						cacheLength:0, 
 						max: 20, 
@@ -66,13 +63,13 @@
 				});
  		 });
 	</script>
-	<input type='submit' value='Ajouter'/>
+	<input type='submit' value='Ajouter' class='btn'/>
 </form>
 </div>
 <?php endif;?>
 
 
-<div class="box_contenu">
+<div class="box">
 <h2>Partage</h2>
 
 <?php if ($infoGroupe['partage']) : ?>
@@ -83,7 +80,7 @@ pour leur propre mail.</p>
 <form action='mailsec/partage-groupe.php' method='post' >		
 	<input type='hidden' name='id_e' value='<?php echo $id_e ?>' />
 	<input type='hidden' name='id_g' value='<?php echo $id_g ?>' />
-	<input type='submit' value='Supprimer le partage'/>
+	<input type='submit' value='Supprimer le partage' class='btn'/>
 </form>
 <?php else:?>
 <div class='box_info'>
@@ -92,7 +89,7 @@ pour leur propre mail.</p>
 <form action='mailsec/partage-groupe.php' method='post' >		
 	<input type='hidden' name='id_e' value='<?php echo $id_e ?>' />
 	<input type='hidden' name='id_g' value='<?php echo $id_g ?>' />
-	<input type='submit' value='Partager'/>
+	<input type='submit' value='Partager' class='btn'/>
 </form>
 <?php endif;?>
 
