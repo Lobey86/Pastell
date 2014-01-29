d95f0029533ceabd8726425e76f09ae1b50bcfcb
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-01-29 09:04:24 +0000

    Branche destinée aux usages Berger-Levrault, permettant : - de soustraire les éléments connecteurs et flux non retenus pour ces usages, et actuellement présents dans le trunk. - d'en maîtriser le contenu et le timing de livraison, sans perturber le trunk, ni être perturbé par le trunk. - de valider, avant leur intégration dans le trunk, les extensions, adaptations et nouveaux mécanismes "core", conçus de manière générique, mais qui pourront nécessiter des compléments, la contribution BL visant prioritairement à couvrir ses propres besoins.

---
+++ V1.2/template/ConnexionNoDroit.php
@@ -0,0 +1,6 @@
+
+<div class="box_error">
+	<p>
+		Vous n'avez aucun droit sur cette plateforme, veuillez contacter votre administrateur PASTELL.
+	</p>
+</div>
---
+++ V1.2/template/UtilisateurModifPassword.php
@@ -0,0 +1,30 @@
+<div class='lien_retour'>
+	<a href='utilisateur/moi.php'>« Espace utilisateur</a>
+</div>
+<div class="box_contenu clearfix">
+
+<h2>Modifier votre mot de passe</h2>
+<form class="w700"  action='utilisateur/modif-password-controler.php' method='post' >
+<table >
+
+<tr>
+<th>Ancien mot de passe : </th>
+<td><input type='password' name='old_password'/></td>
+</tr>
+
+<tr>
+<th>Nouveau mot de passe : </th>
+<td><input type='password' name='password'/></td>
+</tr>
+
+<tr>
+<th>Confirmer le nouveau mot de passe : </th>
+<td><input type='password' name='password2'/></td>
+</tr>
+
+
+</table>
+<input type='submit' value='Modifier le mot de passe' />
+</form>
+
+</div>
