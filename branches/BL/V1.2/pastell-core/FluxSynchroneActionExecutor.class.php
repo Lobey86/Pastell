9488da8fe96bd6b82d9a1b67b1a30733257a25bf
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-01-30 17:08:54 +0000

    - merge 1.2 Adullact : backport BL/1.1/rev760 	- m41535 : permettre l'ajout d'informations statistiques, paramétrables par action 	- m41536 : informations statistiques : taille des dossiers 	- m40090 : les suspensions s'effectuent dans tous les contextes (automatiques, api, ihm) 	- m42261 : on journalise seulement si le flux change d'état. On optimise ainsi les volumes en éliminant les traces des actions automatiques "stériles", , et on conserve la trace des actions déclenchées depuis l'extérieur (console, API). 	- m42413 : erreurs d'encodage apparaissent dans les logs 	- m42808 : ajout de la date dans le détail des erreurs sur action

--- branches/BL/V1.2/pastell-core/FluxSynchroneActionExecutor.class.php
+++ branches/BL/V1.2/pastell-core/FluxSynchroneActionExecutor.class.php
@@ -8,6 +8,7 @@ require_once(PASTELL_PATH . "/pastell-core/ActionExecutor.class.php");
  *   suspension/reprises du connecteur concerné
  * - la détection des services désactivés
  * - le log dans le journal, avec mention de l'appelant (application métier (api, cron) ou console)
+ * - et d'informations complémentaires, à but statistiques essentiellement, et paramétrables par action
  * - la distinction entre actions de workflow (pouvant modifier l'état) et 
  *   actions de recueil d'information (obtention sans modification)
  * - la redirection éventuelle
@@ -19,6 +20,7 @@ require_once(PASTELL_PATH . "/pastell-core/ActionExecutor.class.php");
  * - redéfinition du formattage d'affichage par défaut, pour les actions d'obtention d'information
  */
 abstract class FluxSynchroneActionExecutor extends ActionExecutor {
+
     // Attributs génériques de flux 
 
     const FLUX_ATTR_PILOTE = 'app_pilote';
@@ -29,10 +31,15 @@ abstract class FluxSynchroneActionExecutor extends ActionExecutor {
     const GO_KEY_ETAT = 'goEtat';
     const GO_KEY_MESSAGE = 'goMessage';
     const GO_KEY_REDIRECT = 'goRedirect';
+    const GO_KEY_JOURNALINFOS = 'goInfos';
     // Valeurs conventionnées pour GO_KEY_*
     const GO_ETAT_INCHANGE = 'etat-inchange';
     const GO_ETAT_OK = 'etat-action';
     const GO_MESSAGE_ACTION = 'action-name';
+    // Attributs pour le message du journal
+    const KEY_JOURNAL_PILOTE = 'app';
+    const KEY_JOURNAL_DOCTAILLE = 'taille';
+    const KEY_JOURNAL_MESSAGE = 'msg';
 
     private $fluxActions;
     private $workflowActions;
@@ -102,6 +109,7 @@ abstract class FluxSynchroneActionExecutor extends ActionExecutor {
             }
             $gofEtat = $gof[self::GO_KEY_ETAT];
             $gofMessage = @$gof[self::GO_KEY_MESSAGE];
+            $gofJournalInfos = @$gof[self::GO_KEY_JOURNALINFOS];
             if ($gofEtat == self::GO_ETAT_INCHANGE) {
                 $goRet = true;
                 $goEtat = null;
@@ -115,11 +123,14 @@ abstract class FluxSynchroneActionExecutor extends ActionExecutor {
                 $goEtat = $gofEtat;
                 $goMessage = $gofMessage && ($gofMessage != self::GO_MESSAGE_ACTION) ? $gofMessage : $this->getActionName($goEtat);
             }
-            if ($this->isWorkflow()) {
-                $this->logAction($goEtat, $goMessage);
+            $actionAuto = $this->isActionAuto();
+            // On ne journalise pas les actions automatiques qui ne changent pas l'état.
+            // On optimise ainsi les volumes en éliminant les traces des actions automatiques "stériles".
+            if (!($actionAuto && ($gofEtat == self::GO_ETAT_INCHANGE))) {
+                $this->logAction($goEtat, $goMessage, $gofJournalInfos);
             }
             // En contexte console, conversion pour affichage.
-            if (!$this->isActionAuto() && !$this->from_api) {
+            if (!$actionAuto && !$this->from_api) {
                 $goMessage = $this->goFonctionnelDisplay($goMessage);
             }
             $this->setLastMessage($goMessage);
@@ -140,23 +151,15 @@ abstract class FluxSynchroneActionExecutor extends ActionExecutor {
             // Erreur tracée, état inchangé
             $this->throwException($gofEx, false);
         } catch (ConnecteurAccesException $gofEx) {
-            // La suspension du connecteur s'effectue en contexte asynchrone (appels par cron),
-            // mais pas en contexte synchrone (appels par api ou par ihm).
-            if ($this->isActionAuto()) {
-                // Erreur d'accès en contexte "cron" 
-                try {
-                    // Gestion des suspensions
-                    $this->objectInstancier->ConnecteurSuspensionControler->onAccesEchec($gofEx->getConnecteur());
-                } catch (Exception $onAccesEchecEx) {
-                    // Erreur de gestion des suspensions => erreur tracée, état d'erreur
-                    $this->throwException($onAccesEchecEx, true);
-                }
-                // Erreur tracée, état inchangé
-                $this->throwException($gofEx, false);
-            } else {
-                // Erreur d'accès en contexte "synchrone" => erreur tracée, état inchangé
-                $this->throwException($gofEx, false);
+            // Gestion des suspensions
+            try {
+                $this->objectInstancier->ConnecteurSuspensionControler->onAccesEchec($gofEx->getConnecteur(), $gofEx);
+            } catch (Exception $onAccesEchecEx) {
+                // Erreur de gestion des suspensions => erreur tracée, état d'erreur
+                $this->throwException($onAccesEchecEx, true);
             }
+            // Erreur tracée, état inchangé
+            $this->throwException($gofEx, false);
         } catch (Exception $gofEx) {
             // Erreur fonctionnelle => erreur tracée, état d'erreur
             $this->throwException($gofEx, true);
@@ -170,11 +173,12 @@ abstract class FluxSynchroneActionExecutor extends ActionExecutor {
             $this->logAction($etat, $messageLog);
         }
         $messageDetail = array(
+            'date' => date('d/m/Y H:i:s'),
             'code' => $ex->getCode(),
             'file' => $ex->getFile(),
             'line' => $ex->getLine(),
             'message' => utf8_encode($ex->getMessage()),
-            'trace' => $ex->getTrace());
+            'trace' => utf8_encode_array($ex->getTrace()));
         $messageDetail = json_encode($messageDetail);
         $doc = $this->getDonneesFormulaire();
         $doc->addFileFromData(self::FLUX_ATTR_ERREUR_DETAIL, 'erreur_detail', $messageDetail);
@@ -215,8 +219,8 @@ abstract class FluxSynchroneActionExecutor extends ActionExecutor {
         return $actions->getActionName($action);
     }
 
-    private function logAction($action, $messageLog) {
-        $message = $this->getMessageJournal($messageLog);
+    private function logAction($action, $messageLog, $infos = NULL) {
+        $message = $this->getMessageJournal($messageLog, $infos);
         if ($action) {
             $this->getActionCreator()->addAction($this->id_e, $this->id_u, $action, $message);
         } else {
@@ -235,11 +239,22 @@ abstract class FluxSynchroneActionExecutor extends ActionExecutor {
         return $pilote;
     }
 
-    protected function getMessageJournal($message) {
-        // Note : troncature de fin possible; donc terminer par le message.
+    protected function getMessageJournal($message, $infos = NULL) {
+        // Note : le champ message dans la BD étant limité en taille, il y a risque de troncature de fin; 
+        // on termine donc toujours par le message, les informations statistiques étant prioritaires.
+        $log = '';
+        if (!isset($infos)) {
+            $infos = array();
+        }
+        $pilote = @$infos[self::KEY_JOURNAL_PILOTE];
+        if (!isset($pilote)) {
         $pilote = $this->getPilote();
-        $log = 'app:' . $pilote
-                . ',msg:' . $message;
+            $infos = array(self::KEY_JOURNAL_PILOTE => $pilote) + $infos;
+        }
+        foreach ($infos as $key => $value) {
+            $log .= $key . ':' . $value . ',';
+        }
+        $log .= self::KEY_JOURNAL_MESSAGE . ':' . $message;
         return $log;
     }
 
@@ -317,7 +332,7 @@ abstract class FluxSynchroneActionExecutor extends ActionExecutor {
             $zenMail->setContenuText($contenu);
         }
         $zenMail->send();
-        $this->getJournal()->addSQL(Journal::DOCUMENT_ACTION, $this->id_e, $this->id_u, $this->id_d, $action, 'Notification envoyée à ' . $email);
+        $this->getJournal()->addSQL(Journal::NOTIFICATION, $this->id_e, $this->id_u, $this->id_d, $action, 'Notification envoyée à ' . $email);
     }
 
 }
