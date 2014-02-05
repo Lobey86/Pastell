f51acba88f8d48b5b1327d2396c7ee1c0d292295
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-02-05 13:17:37 +0000

    branche terminée.

--- 1.1/web/api/soap-service.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("init-api.php");
-
-ini_set("soap.wsdl_cache_enabled", 0);
-
-$server = new SoapServer("http://192.168.1.5/adullact/pastell/web/api/wsdl.php");
-$server->setClass("APIExecutor",$apiAction);
-$server->handle();
--- 1.1/web/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__)."/init-authenticated.php");
-
-header("Location: document/index.php");
