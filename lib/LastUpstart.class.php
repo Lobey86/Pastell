0b42475b68dcc841a9f74ee4cdf1d40ca02cea16
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-02-17 07:53:44 +0000

    Ajout de bouton permettant de tuer le script d'action automatique via l'interface

--- lib/LastUpstart.class.php
+++ lib/LastUpstart.class.php
@@ -9,23 +9,58 @@ class LastUpstart {
 		$this->upstart_time_send_warning = $upstart_time_send_warning;
 	}
 	
-	public function updateMtime(){
-		touch($this->upstart_touch_file);
+	public function updatePID(){
+		$info = $this->getInfo();
+		$info['pid'] = getmypid();
+		$this->saveInfo($info);
 	}
 	
+
+	public function deletePID(){
+		$info = $this->getInfo();
+		$info['pid'] = false;
+		$this->saveInfo($info);
+	}
+	
+	private function saveInfo(array $info){
+		file_put_contents($this->upstart_touch_file, serialize($info));
+	}
+	
+	private function getInfo(){
+		$content = file_get_contents($this->upstart_touch_file);
+		if (! $content){
+			return array('pid'=>false,'time'=>0);
+		}
+		return unserialize($content);
+	}
+	
+	public function updateMtime(){		
+		$info = $this->getInfo();
+		$info['time'] = time();
+		$this->saveInfo($info);
+	}
+	
+	
+	
 	public function getLastMtime(){
-		if (file_exists($this->upstart_touch_file)) {
-			return date("Y-m-d H:i:s",filemtime($this->upstart_touch_file));
-		} else {
+		$info = $this->getInfo();
+		if (! $info['time']) {
 			return false;
 		}
+		return date("Y-m-d H:i:s",$info['time']);
 	}
 	
 	public function hasWarning() {
-		if (! file_exists($this->upstart_touch_file)) {
+		$info = $this->getInfo();
+		if (! $info['time']) {
 			return true;
 		}
-		return (time() - filemtime($this->upstart_touch_file)) >= $this->upstart_time_send_warning;
+		return (time() - $info['time']) >= $this->upstart_time_send_warning;
+	}
+	
+	public function getPID(){
+		$info = $this->getInfo();
+		return $info['pid'];
 	}
 	
 	
