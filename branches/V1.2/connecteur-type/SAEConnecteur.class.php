f843688683da4e024a30f55a553e4591bd7a8bdc
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-03-21 06:40:08 +0000

    - Correction de SAEConnecteur pour tester le retour de la commande tar de manière correcte - connecteur Asal@e-rest : ajout des dernier bordereau/archive envoyé

--- branches/V1.2/connecteur-type/SAEConnecteur.class.php
+++ branches/V1.2/connecteur-type/SAEConnecteur.class.php
@@ -11,10 +11,12 @@ abstract class SAEConnecteur extends  Connecteur {
 		}
 		$fileName = uniqid()."_archive.tar.gz";
 		$command = "tar cvzf $tmp_folder/$fileName --directory $tmp_folder \"" . implode("\" \"",$file_to_add) ."\"";
-		$status = exec($command );
-		if (! $status){
-			throw new Exception("Impossible de créer le fichier d'archive $fileName - Commande : $command");
+		$status = exec($command,$output,$return_var);
+				
+		if ( $return_var != 0) {
+			throw new Exception("Impossible de créer le fichier d'archive $fileName - status : $return_var");
 		}
+		
 		return $tmp_folder."/$fileName";
 	}	
 	
