5b64b8bf4c515e532d59128de2e6257c0e2d3578
pviver <pviver@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-03-31 13:56:39 +0000

    Version 1.2.03 revision 797 de Pastell

--- V1.2/web/api/detail-document.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("init-api.php");
-
-$recuperateur = new Recuperateur($_REQUEST);
-$id_e = $recuperateur->get('id_e');
-$id_d = $recuperateur->get('id_d');
-
-$api_json->detailDocument($id_e, $id_d);
