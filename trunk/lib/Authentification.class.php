d6cfd5c483c03366d8c30e33ca2223c655fb3962
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-04-08 08:32:16 +0000

    Correction du bug 2687 : Le breadrcrumbs n'était pas correctement calculé dans le cas où il y avait deux collectivité mère fille et que l'on choissisait un document via l'accueil : la liste des documents affichait la mère et le breacrumbs n'affichait pas la liste.

--- trunk/lib/Authentification.class.php
+++ trunk/lib/Authentification.class.php
@@ -4,7 +4,6 @@ class Authentification {
 	public function connexion($login,$id_u){
 		$_SESSION['connexion']['login'] = $login;
 		$_SESSION['connexion']['id_u'] = $id_u;
-		$_SESSION['connexion']['breadcrumbs'] = array();
 	}
 	
 	public function isConnected(){
