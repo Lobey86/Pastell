50e3fa444bf7f76b9d7c7bc149c4537127f274a5
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-04-18 14:25:09 +0000

    - m44779 : éviter les erreurs "No entry for terminal type "unknown";" lors de l'exécution par cron

--- branches/BL/1.2/bl-core-extension/batch/bl-demande-classification.sh
+++ branches/BL/1.2/bl-core-extension/batch/bl-demande-classification.sh
@@ -2,6 +2,12 @@
 # ATTENTION : utiliser les sauts de ligne LINUX et pas WINDOWS
 
 echo [$(date +%d-%m-%Y\ %H\:%M\:%S)] Demarrage $0
+
+# éviter les erreurs "No entry for terminal type "unknown";" lors de l'exécution par cron
+if [ ! $TERM ]; then
+    export TERM=xterm
+fi
+
 if [ ! -z $(find /tmp -maxdepth 1 -iname '*.lock' -print -quit) ]
 then
     echo Application en cours de maintenance.
