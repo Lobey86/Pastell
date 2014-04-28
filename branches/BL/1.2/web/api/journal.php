f05f5534c9b7ec53e8e2578d944809ffa3578183
gdon <gdon@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-04-28 09:28:59 +0000

    mantis 38357 : WS Journal.php -  Pour éviter les problèmes mémoires, l'écriture du fichier CSV se fait par paquet.

--- branches/BL/1.2/web/api/journal.php
+++ branches/BL/1.2/web/api/journal.php
@@ -13,18 +13,62 @@ $recherche = $recuperateur->get('recherche');
 $date_debut = $recuperateur->get('date_debut');
 $date_fin = $recuperateur->get('date_fin');
 $format = $recuperateur->get('format');
+$nbreLigneMaxParPaquet = $recuperateur->get('nbreLigneMaxParPaquet', 10000);
 
 if   (! $roleUtilisateur->hasDroit($id_u,"journal:lecture",$id_e)){
 	$JSONoutput->displayErrorAndExit("Acces interdit id_e=$id_e, id_d=$id_d,id_u=$id_u,type=$type");
 }
 
-$all = $journal->getAll($id_e,$type,$id_d,$id_user,$offset,$limit,$recherche,$date_debut,$date_fin) ;
+// Pour éviter des problèmes mémoires, au format CSV : 
+//  - le chargement des lignes se fait par paquet.
+//  - les lignes sont écrites dans le fichier CSV au fur et à mesure des chargements des paquets.
+// NB : Le problème "mémoire", existe toujours pour le format JSON.
 
+if ($format == 'csv') {
+    if ($limit!=-1 && $limit < $nbreLigneMaxParPaquet) {    
+        $nbreLigne=$limit;
+    } else {
+        $nbreLigne=$nbreLigneMaxParPaquet;
+    }
+    $ligneDepart = $offset;
+    $all = array();
+    $filecsv = tempnam('/tmp/', 'exportjournal');
+    $handle = fopen($filecsv, 'w');
+    $continuer = true;
+    while ($continuer) {
+        $tab = $journal->getAll($id_e, $type, $id_d, $id_user, $ligneDepart, $nbreLigne, $recherche, $date_debut, $date_fin, true);
+        $nbreLigne = sizeof($tab);
+        $ligneDepart = $ligneDepart + $nbreLigne;
+        foreach ($tab as $ligne) {
+            $ligne['message'] = preg_replace("/(\r\n|\n|\r)/", " ", $ligne['message']);
+            $ligne['message_horodate'] = preg_replace("/(\r\n|\n|\r)/", " ", $ligne['message_horodate']);
+            unset($ligne['preuve']);
+            fputcsv($handle, $ligne);
+        }
+        $tab = null;        
+        $continuer = (($limit==-1) || ($limit > $nbreLigneMaxParPaquet)) && ($nbreLigne == $nbreLigneMaxParPaquet);
+        if( $continuer) {
+            // Calcul du nombre de ligne de la prochaine itération.
+            if (($limit != -1) && (($ligneDepart + $nbreLigne) > $limit)) {
+                $nbreLigne = $limit - $ligneDepart;
+            }
+        }
+    }   
+    fclose($handle);
+    header("Content-type: text/csv; charset=iso-8859-1");
+    header("Content-disposition: attachment; filename=pastell-export-journal-$id_e-$type-$id_d.csv");
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
+    header("Pragma: public");
+    readfile($filecsv);
 
-if ($format == 'csv'){
-	$CSVoutput = new CSVoutput();
-	$CSVoutput->sendAttachment("pastell-export-journal-$id_e-$type-$id_d.csv",$all);
-
+    unlink($filecsv);
 } else {
-	$JSONoutput->display($all);
+    $all = $journal->getAll($id_e, $type, $id_d, $id_user, $offset, $limit, $recherche, $date_debut, $date_fin);
+    if ($format == 'csv') {
+        $CSVoutput = new CSVoutput();
+        $CSVoutput->sendAttachment("pastell-export-journal-$id_e-$type-$id_d.csv", $all);
+    } else {
+        $JSONoutput->display($all);
+    }
 }
