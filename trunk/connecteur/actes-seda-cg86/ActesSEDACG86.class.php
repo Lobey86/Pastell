243fe08eacd0ee6ef4d93f9da822b147a922bd4c
pviver <pviver@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-05-02 14:46:50 +0000

    bugfix : les classifications n'étaient pas celle du Thesaurus

--- trunk/connecteur/actes-seda-cg86/ActesSEDACG86.class.php
+++ trunk/connecteur/actes-seda-cg86/ActesSEDACG86.class.php
@@ -99,7 +99,9 @@ class ActesSEDACG86  extends SEDAConnecteur {
 		$ar_actes_info = $this->getInfoARActes($transactionsInfo['ar_actes']);
 		
 		$latestDate = $this->getLatestDate($transactionsInfo);
-		 
+		
+		$transactionsInfo['classification_tab'] = explode('.',$transactionsInfo['classification']);
+		
 		$archiveTransfer = new ZenXML('ArchiveTransfer');
 		$archiveTransfer['xmlns'] = "fr:gouv:ae:archive:draft:standard_echange_v0.2";
 		$archiveTransfer->Comment = "Transfert d'un acte soumis au contrôle de légalité";
@@ -202,7 +204,7 @@ class ActesSEDACG86  extends SEDAConnecteur {
 		$archiveTransfer->Contains->ContentDescription->ContentDescriptive[2]->KeywordType = "genreform";
 		$archiveTransfer->Contains->ContentDescription->ContentDescriptive[2]->KeywordType["listVersionID"] = "edition 2009";
 		
-		if ($transactionsInfo['classification'][0] != 9 ){
+		if ($transactionsInfo['classification_tab'][0] != 9 ){
 			$archiveTransfer->Contains->ContentDescription->ContentDescriptive[3]->KeywordContent = $this->getSujetActes($transactionsInfo['classification']);
 			$archiveTransfer->Contains->ContentDescription->ContentDescriptive[3]->KeywordReference['schemeName'] = "Thésaurus pour la description et l'indexation des archives locales anciennes, modernes et contemporaines_liste d'autorité Actions";
 			$archiveTransfer->Contains->ContentDescription->ContentDescriptive[3]->KeywordReference['schemeAgencyName'] = "Direction des Archives de France";	
@@ -217,7 +219,7 @@ class ActesSEDACG86  extends SEDAConnecteur {
 		$archiveTransfer->Contains->Appraisal->Duration = "P1Y";
 		$archiveTransfer->Contains->Appraisal->StartDate = date('Y-m-d',strtotime($latestDate));
 
-		$archiveTransfer->Contains->AccessRestriction->Code = $this->getAccessRestriction($transactionsInfo['classification'],$transactionsInfo['nature_code']);
+		$archiveTransfer->Contains->AccessRestriction->Code = $this->getAccessRestriction($transactionsInfo['classification_tab'],$transactionsInfo['nature_code']);
 		$archiveTransfer->Contains->AccessRestriction->Code['listVersionID'] = "edition 2009";
 		$archiveTransfer->Contains->AccessRestriction->StartDate = date('Y-m-d',strtotime($latestDate));
 		
@@ -426,72 +428,31 @@ class ActesSEDACG86  extends SEDAConnecteur {
 	
 	private function getSujetActes($classification){
 
-		$info = array(	"1" => "Commande publique",
-				"1.1" => "Marches publics",
-				"1.2" => "Delegation de service public",
-				"1.3" => "Conventions de Mandat",
-				"1.4" => "Autres types de contrats",
-				"1.5" => "Transactions /protocole d accord transactionnel",
-				"1.6" => "Actes relatifs à la maitrise d oeuvre",
-				"1.7" => "Actes speciaux et divers",
-						"2" => "Urbanisme", 
-				"2.1" => "Documents d urbanisme",
-				"2.2" => "Actes relatifs au droit d occupation ou d utilisation des sols",
-				"2.3" => "Droit de preemption urbain", 
-						"3" => "Domaine et patrimoine",
-				"3.1" => "Acquisitions",
-				"3.2" => "Alienations",
-				"3.3" => "Locations",
-				"3.4" => "Limites territoriales",
-				"3.5" => "Autres actes de gestion du domaine public",
-				"3.6" => "Autres actes de gestion du domaine prive",
-						"4" => "Personnel",
-				"4.1" => "Personnel titulaires et stagiaires de la F.P.T.",
-				"4.2" => "Personnel contractuel",
-				"4.3" => "Fonction publique hospitaliere",
-				"4.4" => "Autres categories de personnels",
-				"4.5" => "Regime indemnitaire",
-						"5" => "Election politique, Collectivité locale",
-				"5.1" => "Election executif",
-				"5.2" => "Fonctionnement des assemblees",
-				"5.3" => "Designation de representants",
-				"5.4" => "Delegation de fonctions",
-				"5.5" => "Delegation de signature",
-				"5.6" => "Exercice des mandats locaux",
-				"5.7" => "Intercommunalite",
-				"5.8" => "Decision d ester en justice",
-						"6" => "Police, Protection civile",
-				"6.1" => "Police municipale",
-				"6.2" => "Pouvoir du president du conseil general",
-				"6.3" => "Pouvoir du president du conseil regional",
-				"6.4" => "Autres actes reglementaires",
-				"6.5" => "Actes pris au nom de l Etat et soumis au controle hierarchique",
-						"7" => "Finances locales",
-				"7.1" => "Decisions budgetaires",
+		$info = array(  "1" => "Commande publique",
+				"2" => "Urbanisme",
+				"3" => "Propriété publique",
+				"3.4" => "Circonscription territoriale",
+				"4" => "Personnel",
+				"5" => "Election politique, Collectivité locale",
+				"5.2" => "Organe délibérant",
+				"5.6" => "Elu",
+				"5.7" => "Etablissement public de coopération intercommunale",
+				"5.8" => "Justice",
+				"6" => "Police, Protection civile",
+				"7" => "Finances locales",
+				"7.1" => "Comptabilité publique",
 				"7.2" => "Fiscalité",
-				"7.3" => "Emprunts",
-				"7.4" => "Interventions economiques",
-				"7.5" => "Subventions",
-				"7.6" => "Contributions budgetaires",
-				"7.7" => "Avances",
-				"7.8" => "Fonds de concours",
-				"7.9" => "Prise de participation (SEM, etc...)",
-				"7.10" => "Divers",
-						"8" => "Education",
-				"8.1" => "Enseignement",
-				"8.2" => "Aide sociale",
-				"8.3" => "Voirie",
+				"7.3" => "Dette publique",
+				"7.6" => "Comptabilité publique",
+				"8.1" => "Education",
+				"8.2" => "Protection sociale",
+				"8.3" => "Réseau routier",
 				"8.4" => "Amenagement du territoire",
-				"8.5" => "Politique de la ville-habitat-logement",
-				"8.6" => "Emploi-formation professionnelle",
-				"8.7" => "Transports",
+				"8.5" => "Politique de la ville, Immobilier",
+				"8.6" => "Emploi",
+				"8.7" => "Transport",
 				"8.8" => "Environnement",
-				"8.9" => "Culture",
-				"9" => "Autres domaines de competences",
-				"9.1" => "Autres domaines de competences des communes",
-				"9.2" => "Autres domaines de competences des departements",
-				"9.3" => "Autres domaines de competences des regions",
-				"9.4" => "Voeux et motions");
+				"8.9" => "Culture");
 		$debut = substr($classification,0,3);
 		
 		if (isset($info[$debut])){
