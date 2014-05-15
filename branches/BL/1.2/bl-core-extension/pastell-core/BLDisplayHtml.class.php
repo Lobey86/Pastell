ea7407934120bb2611b984090641bf682cc60297
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-05-15 09:31:10 +0000

    - prise en compte des structures multi-niveaux, tableaux ou objets

--- branches/BL/1.2/bl-core-extension/pastell-core/BLDisplayHtml.class.php
+++ branches/BL/1.2/bl-core-extension/pastell-core/BLDisplayHtml.class.php
@@ -1,12 +1,20 @@
 <?php
 
-require_once(__DIR__ . "/BLDisplayText.class.php");
+require_once(__DIR__ . "/BLDisplayValue.class.php");
 
-class BLDisplayHtml extends BLDisplayText {
+class BLDisplayHtml extends BLDisplayValue {
 
     protected function encode($text) {
         $result = htmlentities($text);
         return $result;
+    }
+
+    protected function concat(&$target, &$source) {
+        if ($target) {
+            $target .= '<br>';
+        }
+        $target .= $source;
+        return $target;
     }
 
     protected function formatCellHeader($colIndex, $colName) {
@@ -15,8 +23,7 @@ class BLDisplayHtml extends BLDisplayText {
     }
 
     protected function formatRowHeader($rowValue) {
-        $result = '<tr align="left">' . $rowValue . '</tr>';
-        return $result;
+        return $this->formatRow(0, $rowValue);
     }
 
     protected function formatCell($rowIndex, $colIndex, $colName, $cellValue) {
@@ -29,20 +36,149 @@ class BLDisplayHtml extends BLDisplayText {
         return $result;
     }
 
-    protected function formatTable($tableValue) {
-        $border = $this->hasHeader ? '1' : '0';
-        $result = '<table border=' . $border . ' cellspacing=0>'
-                . $tableValue
-                . '</table>';
+    protected function formatTable($headerValue, $bodyValue) {
+        $hasHeader = !empty($headerValue);
+        $hasBody = !empty($bodyValue);
+        $result = '';
+        if ($hasHeader || $hasBody) {
+            $result = '<table border=1 cellspacing=0>';
+        }
+        if ($hasHeader) {
+            $result .= '<thead align="left">' . $headerValue . '</thead>';
+        }
+        if ($hasBody) {
+            $result .= '<tbody valign="top">' . $bodyValue . '</tbody>';
+        }
+        if ($hasHeader || $hasBody) {
+            $result .= '</table>';
+        }
         return $result;
     }
 
-    protected function concat(&$target, &$source) {
-        if ($target) {
-            $target .= '<br>';
+    private function elemColumns($elem) {
+        if (is_object($elem)) {
+            $columns = array();
+            foreach ($elem as $th => $td) {
+                $columns[] = $th;
+            }
+        } elseif (is_array($elem)) {
+            $columns = array_keys($elem);
+        } else {
+            $columns = null;
         }
-        $target .= $source;
-        return $target;
+        return $columns;
+    }
+
+    private function headerDisplay($columns, $doEncoding = true) {
+        $rowValue = '';
+        $colIndex = 0;
+        foreach ($columns as $colName) {
+            $colNameValue = $this->stringDisplay($colName, $doEncoding);
+            $cellDisplay = $this->formatCellHeader($colIndex, $colNameValue);
+            if ($cellDisplay) {
+                $rowValue .= $cellDisplay;
+            }
+            $colIndex++;
+        }
+        $rowDisplay = $this->formatRowHeader($rowValue);
+        return $rowDisplay;
+    }
+
+    private function cellDisplay($rowIndex, $colIndex, $colName, $cellValue, $doEncoding = true) {
+        $colNameValue = $this->stringDisplay($colName, $doEncoding);
+        $cellValue = $this->valueDisplay($cellValue, $doEncoding);
+        $cellDisplay = $this->formatCell($rowIndex, $colIndex, $colNameValue, $cellValue);
+        return $cellDisplay;
+    }
+
+    protected function objectDisplay($object, $doEncoding = true) {
+        $columns = $this->elemColumns($object);
+        $hasColumns = !empty($columns);
+        $hasHeader = $hasColumns && is_string($columns[0]);
+        $headerValue = '';
+        if ($hasHeader) {
+            $headerValue = $this->headerDisplay($columns, $doEncoding = true);
+        }
+
+        $colIndex = 0;
+        $rowValue = '';
+        foreach ($object as $colName => $cellValue) {
+            $rowValue .= $this->cellDisplay(0, $colIndex, $colName, $cellValue, $doEncoding);
+            $colIndex++;
+        }
+        $bodyValue = '';
+        if (!empty($rowValue)) {
+            $bodyValue = $this->formatRow(0, $rowValue);
+        }
+
+        $tableDisplay = $this->formatTable($headerValue, $bodyValue);
+        return $tableDisplay;
+    }
+
+    protected function arrayDisplay(array $array, $doEncoding = true) {
+        if (empty($array)) {
+            $tableDisplay = $this->stringDisplay('aucun élément', $doEncoding);
+        } else {
+            // Les titres des colonnes sont affichées si le tableau est multi-colonnes;
+            // c'est le cas si les lignes elles-mêmes sont des tableaux ou des objets.
+            // Les titres des colonnes sont alors les keys ou les attributs de la première ligne.
+            // Toutes les lignes sont censées avoir la même structure; le comportement n'est pas
+            // déterminé dans le cas contraire.
+            $headerValue = '';
+            $bodyValue = '';
+
+            $arrayKeys = array_keys($array);
+            $firstKey = $arrayKeys[0];
+            $isListe = !is_string($firstKey);
+            if ($isListe) {
+                $firstElem = $array[$firstKey];
+                $columns = $this->elemColumns($firstElem);
+                $hasColumns = !empty($columns);
+                $hasHeader = $hasColumns && is_string($columns[0]);
+                if ($hasHeader) {
+                    $headerValue = $this->headerDisplay($columns, $doEncoding);
+                }
+                $rowIndex = 0;
+                foreach ($array as $tr) {
+                    if ($hasColumns) {
+                        $isObject = is_object($tr);
+                        $colIndex = 0;
+                        $rowValue = '';
+                        foreach ($columns as $colName) {
+                            if ($isObject) {
+                                $cellValue = isset($tr->{$colName}) ? $tr->{$colName} : '';
+                            } else {
+                                $cellValue = isset($tr[$colName]) ? $tr[$colName] : '';
+                            }
+                            $rowValue .= $this->cellDisplay($rowIndex, $colIndex, $colName, $cellValue, $doEncoding);
+                            $colIndex++;
+                        }
+                    } else {
+                        $rowValue = $this->cellDisplay($rowIndex, 0, null, $tr, $doEncoding);
+                    }
+                    $rowDisplay = $this->formatRow($rowIndex, $rowValue);
+                    $bodyValue .= $rowDisplay;
+                    $rowIndex++;
+                }
+            } else {
+                $columns = $this->elemColumns($array);
+                $hasColumns = !empty($columns);
+                $hasHeader = $hasColumns && is_string($columns[0]);
+                if ($hasHeader) {
+                    $headerValue = $this->headerDisplay($columns, $doEncoding);
+                }
+                $colIndex = 0;
+                $rowValue = '';
+                foreach ($array as $colName => $cellValue) {
+                    $rowValue .= $this->cellDisplay(0, $colIndex, $colName, $cellValue, $doEncoding);
+                    $colIndex++;
+                }
+                $rowDisplay = $this->formatRow(0, $rowValue);
+                $bodyValue .= $rowDisplay;
+            }
+            $tableDisplay = $this->formatTable($headerValue, $bodyValue);
+        }
+        return $tableDisplay;
     }
 
     public function hyperlinkDisplay($value, $url, $doEncoding = true) {
