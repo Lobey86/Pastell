4463ea071a98c80e1e6c26dcdf4ca80689a7e6cf
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-05-19 15:58:50 +0000

    - m45079 : connecteur SRCI Parapheur

--- 1.2/bl-core-extension/installation/BLScriptUpdateVersion_01.php
+++ 1.2/bl-core-extension/installation/BLScriptUpdateVersion_01.php
@@ -1,13 +1,13 @@
 #! /usr/bin/php
-<?php 
+<?php
 require_once( __DIR__ . "/../../web/init.php");
 require_once( __DIR__ . "/../batch/BLBatch.class.php");
 
 $blScript = new BLBatch();
 
-// Suppresion du droit system:modification sur le role adminEntite.
+// Suppression du droit system:modification sur le role adminEntite.
 $blScript->trace('Suppression du droit \'system:edition\' du rôle \'adminEntite\' : ');
-$param =array("adminEntite", "system:edition");
+$param = array("adminEntite", "system:edition");
 $droitExist = $sqlQuery->queryOne("SELECT * FROM role_droit WHERE role=? and droit=?", $param);
 if ($droitExist) {
     $sqlQuery->queryOne("DELETE FROM role_droit WHERE role=? and droit=?", $param);
@@ -18,11 +18,21 @@ if ($droitExist) {
 
 // Mise en place de l'extension BL : Connecteur fasttdtheliosbl
 $blScript->trace('Mise en place extension BL Connecteur fasttdtheliosbl : ');
-$prov_extension = false;
 $requeteExtension = "SELECT id_e FROM extension WHERE path = ?";
 $ext_fasttdtheliosblbl = "/var/www/pastell/extensionbl/fasttdtheliosbl/";
 if (!$sqlQuery->queryOne($requeteExtension, $ext_fasttdtheliosblbl)) {
     $sqlQuery->queryOne("INSERT INTO extension (path) VALUES(?)", $ext_fasttdtheliosblbl);
+    $blScript->traceln('OK');
+} else {
+    $blScript->traceln('DEJA FAIT');
+}
+
+// Mise en place de l'extension BL : Connecteur srciparabl
+$blScript->trace('Mise en place extension BL srciparabl : ');
+$requeteExtension = "SELECT id_e FROM extension WHERE path = ?";
+$ext_srciparabl = "/var/www/pastell/extensionbl/srciparabl/";
+if (!$sqlQuery->queryOne($requeteExtension, $ext_srciparabl)) {
+    $sqlQuery->queryOne("INSERT INTO extension (path) VALUES(?)", $ext_srciparabl);
     $blScript->traceln('OK');
 } else {
     $blScript->traceln('DEJA FAIT');
