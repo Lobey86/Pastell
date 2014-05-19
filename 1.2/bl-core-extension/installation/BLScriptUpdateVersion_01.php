35fe849f3e7a1810fe2eb27a5e51739e6a82c110
gdon <gdon@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-05-19 14:06:12 +0000

    m45083: Connecteur Fast Hélios

--- 1.2/bl-core-extension/installation/BLScriptUpdateVersion_01.php
+++ 1.2/bl-core-extension/installation/BLScriptUpdateVersion_01.php
@@ -3,15 +3,27 @@
 require_once( __DIR__ . "/../../web/init.php");
 require_once( __DIR__ . "/../batch/BLBatch.class.php");
 
-$blscript = new BLBatch();
+$blScript = new BLBatch();
 
-//Suppresion du droit system:modification sur le role adminEntite.
-$blscript->trace('Suppression du droit \'system:edition\' du rôle \'adminEntite\' : ');
+// Suppresion du droit system:modification sur le role adminEntite.
+$blScript->trace('Suppression du droit \'system:edition\' du rôle \'adminEntite\' : ');
 $param =array("adminEntite", "system:edition");
 $droitExist = $sqlQuery->queryOne("SELECT * FROM role_droit WHERE role=? and droit=?", $param);
 if ($droitExist) {
     $sqlQuery->queryOne("DELETE FROM role_droit WHERE role=? and droit=?", $param);
-    $blscript->traceln('OK');
+    $blScript->traceln('OK');
 } else {
-    $blscript->traceln('DEJA FAIT');
+    $blScript->traceln('DEJA FAIT');
+}
+
+// Mise en place de l'extension BL : Connecteur fasttdtheliosbl
+$blScript->trace('Mise en place extension BL Connecteur fasttdtheliosbl : ');
+$prov_extension = false;
+$requeteExtension = "SELECT id_e FROM extension WHERE path = ?";
+$ext_fluxbl = "/var/www/pastell/extensionbl/fasttdtheliosbl/";
+if (!$sqlQuery->queryOne($requeteExtension, $ext_fluxbl)) {
+    $sqlQuery->queryOne("INSERT INTO extension (path) VALUES(?)", $ext_fluxbl);
+    $blScript->traceln('OK');
+} else {
+    $blScript->traceln('DEJA FAIT');
 }
