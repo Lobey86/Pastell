5ec4f84257b81e67fa76aa66dc8b1e6a9deee134
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-05-28 07:59:56 +0000

    Ajout des réponse à un courrier simple

--- connecteur/s2low/S2low.class.php
+++ connecteur/s2low/S2low.class.php
@@ -290,7 +290,7 @@ class S2low  extends TdtConnecteur {
 	}
 	
 	public function sendResponse(DonneesFormulaire $donneesFormulaire) {
-		foreach(array(3,4) as $id_type) {
+		foreach(array(2,3,4) as $id_type) {
 			$libelle = $this->getLibelleType($id_type);
 			if($donneesFormulaire->get("has_$libelle") == true){
 				if ($donneesFormulaire->get("has_reponse_$libelle") == false){
@@ -342,6 +342,7 @@ class S2low  extends TdtConnecteur {
 		$donneesFormulaire->setData("has_reponse_{$libelle}",true);
 		return true;
 	}
+	
 }
 
 class S2lowException extends TdTException {}
