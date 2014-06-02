a713d877741af9a91a7da64c6ae9416119f1ed5c
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-06-02 08:33:47 +0000

    Connecteur Helios SEDA standard

--- connecteur/helios-seda-standard/action/TestBordereau.class.php
+++ connecteur/helios-seda-standard/action/TestBordereau.class.php
@@ -10,10 +10,13 @@ class TestBordereau extends ActionExecutor {
 				'date' => date("Y-m-d"), 
 				'description' => 'bla', 
 				'pes_retour_description' => 'bla', 
-				'pes_aller' => '/etc/passwd',
-				'pes_retour' => '/etc/group',
+				'pes_aller' => __DIR__."/../fixtures/pes_aller.xml",
+				'pes_retour' => __DIR__."/../fixtures/pes_acquit.xml",
 				'pes_description' => 'toto',
+				'pes_aller_content' => file_get_contents(__DIR__."/../fixtures/pes_aller.xml"),
 		);
+		
+		
 		
 		$bordereau = $heliosArchivesSEDA->getBordereau($transactionsInfo);	
 		header("Content-type: text/xml");
