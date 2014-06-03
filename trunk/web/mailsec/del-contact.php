3545062f3ceaf961fabdd018194ceb55a5fbe6bf
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-06-03 08:07:16 +0000

    Correction bug nb utilisateur dans les groupes (Bug 4839 et 4768)

--- trunk/web/mailsec/del-contact.php
+++ trunk/web/mailsec/del-contact.php
@@ -11,7 +11,16 @@ if ( ! $roleUtilisateur->hasDroit($authentification->getId(),"annuaire:edition",
 }
 
 $annuaire = new Annuaire($sqlQuery,$id_e);
-
+$annuaireGroupe = new AnnuaireGroupe($sqlQuery, $id_e);
+foreach ($email as $mail){
+	$id_a = $annuaire->getFromEmail($mail);
+	if ($annuaireGroupe->hasAGroupe($id_a)){
+		$lastError->setLastError("Impossible de supprimer $mail : celui-ci appartient à un groupe");
+		header("Location: annuaire.php?id_e=$id_e");
+		exit;
+	}
+	
+}
 $annuaire->delete($email);
 
 $lastMessage->setLastMessage("Email supprimé de la liste de contacts");
