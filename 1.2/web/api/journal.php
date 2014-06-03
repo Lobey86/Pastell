13cb5520c6b559fd161f45b0c4e50d27ee06306d
gdon <gdon@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-06-03 06:28:09 +0000

    0045355 : Possibilité d'ajouter les entetes de colonne dans le fichier de sortie csv

--- 1.2/web/api/journal.php
+++ 1.2/web/api/journal.php
@@ -13,7 +13,8 @@ $recherche = $recuperateur->get('recherche');
 $date_debut = $recuperateur->get('date_debut');
 $date_fin = $recuperateur->get('date_fin');
 $format = $recuperateur->get('format');
-$nbreLigneMaxParPaquet = $recuperateur->get('nbre_ligne_max_paquet', 10000);
+$nbre_ligne_max_paquet = $recuperateur->get('nbre_ligne_max_paquet', 10000);
+$csv_entete_colonne = $recuperateur->getInt('csv_entete_colonne', 0);
 
 if   (! $roleUtilisateur->hasDroit($id_u,"journal:lecture",$id_e)){
 	$JSONoutput->displayErrorAndExit("Acces interdit id_e=$id_e, id_d=$id_d,id_u=$id_u,type=$type");
@@ -26,30 +27,39 @@ if   (! $roleUtilisateur->hasDroit($id_u,"journal:lecture",$id_e)){
 // NB : Le problème "mémoire", existe toujours pour le format JSON.
 
 if ($format == 'csv') {
-    if ($limit!=-1 && $limit < $nbreLigneMaxParPaquet) {    
+    if ($limit!=-1 && $limit < $nbre_ligne_max_paquet) {    
         $nbreLigne=$limit;
     } else {
-        $nbreLigne=$nbreLigneMaxParPaquet;
+        $nbreLigne=$nbre_ligne_max_paquet;
     }
     $ligneDepart = $offset;
     $all = array();
     $filecsv = tempnam('/tmp/', 'exportjournal');
     $handle = fopen($filecsv, 'w');
     $continuer = true;
-    $max_execution_time= ini_get('max_execution_time');
+    $max_execution_time= ini_get('max_execution_time');    
     while ($continuer) {
         ini_set('max_execution_time', $max_execution_time);
         $tab = $journal->getAll($id_e, $type, $id_d, $id_user, $ligneDepart, $nbreLigne, $recherche, $date_debut, $date_fin, true);
         $nbreLigne = sizeof($tab);
         $ligneDepart = $ligneDepart + $nbreLigne;
         foreach ($tab as $ligne) {
+            if ($csv_entete_colonne) {
+                // Les entêtes sont les clés du tableau associatif
+                $entetes = array_keys($ligne);
+                // Suppression de la colonne preuve
+                $index_col_preuve = array_search('preuve', $entetes, true);
+                array_splice($entetes, $index_col_preuve, 1);                
+                fputcsv($handle, $entetes);
+                $csv_entete_colonne=false;
+            }
             $ligne['message'] = preg_replace("/(\r\n|\n|\r)/", " ", $ligne['message']);
             $ligne['message_horodate'] = preg_replace("/(\r\n|\n|\r)/", " ", $ligne['message_horodate']);
             unset($ligne['preuve']);
             fputcsv($handle, $ligne);
         }
         $tab = null;        
-        $continuer = (($limit==-1) || ($limit > $nbreLigneMaxParPaquet)) && ($nbreLigne == $nbreLigneMaxParPaquet);
+        $continuer = (($limit==-1) || ($limit > $nbre_ligne_max_paquet)) && ($nbreLigne == $nbre_ligne_max_paquet);
         if( $continuer) {
             // Calcul du nombre de ligne de la prochaine itération.
             if (($limit != -1) && (($ligneDepart + $nbreLigne) > $limit)) {
