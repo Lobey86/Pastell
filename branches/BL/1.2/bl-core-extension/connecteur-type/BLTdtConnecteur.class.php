331efbcdb20930e5a6b6b620a4a27fc9ead6e6d5
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-06-06 10:45:00 +0000

    - m45081 : ajout du connecteur ganeshtdtheliosbl - refactoring : utiliser des constantes pour les noms d'attributs

--- branches/BL/1.2/bl-core-extension/connecteur-type/BLTdtConnecteur.class.php
+++ branches/BL/1.2/bl-core-extension/connecteur-type/BLTdtConnecteur.class.php
@@ -10,6 +10,7 @@ abstract class BLTdtConnecteur extends TdtConnecteur {
     const KEY_INFO_FILESIZE = 'filesize';
     // Attributs de flux, spécifiques à ce type de connecteur;
     // accessibles uniquement par les connecteurs de ce type.
+    const ATTR_TDT_TRANSACTION_ID = 'tdt_transaction_id';
     const ATTR_TRANSACTION_ANNULATION_ID = "tdt_transaction_annulation_id";
 
     public function isUsageActe() {
