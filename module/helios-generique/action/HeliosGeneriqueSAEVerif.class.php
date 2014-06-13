8a3041ddb282e9bd35acbb1e2a59fc94c739a370
pviver <pviver@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-06-13 09:01:36 +0000

    correction sur l'identifiant unique

--- module/helios-generique/action/HeliosGeneriqueSAEVerif.class.php
+++ module/helios-generique/action/HeliosGeneriqueSAEVerif.class.php
@@ -5,7 +5,7 @@ class HeliosGeneriqueSAEVerif extends ActionExecutor {
 	public function go(){
 		$sae = $this->getConnecteur('SAE');
 		
-		$id_transfert = $this->getDonneesFormulaire()->get('uniqid');
+		$id_transfert = $this->getDonneesFormulaire()->get('sae_transfert_id');
 		$sae_config = $this->getConnecteurConfigByType('SAE');
 		$ar = $sae->getAcuseReception($id_transfert);
 		
