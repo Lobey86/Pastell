29e7fcea70175d10ee3779fe6a11914ba75e5d84
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-07-04 09:22:47 +0000

    - m43844 : les actions de cloture ne doivent pas être enregistrées dans les tables des documents, ces derniers ayant été supprimés.

--- 1.2/bl-core-extension/installation/BLScriptUpdateVersion_01.php
+++ 1.2/bl-core-extension/installation/BLScriptUpdateVersion_01.php
@@ -1,17 +1,22 @@
-#! /usr/bin/php
 <?php
 require_once( __DIR__ . "/../../web/init.php");
 require_once( __DIR__ . "/../batch/BLBatch.class.php");
 
 $blScript = new BLBatch();
 
-// Mise en place de l'extension BL : Connecteur ganeshtdtactesbl
-$blScript->trace('Mise en place extension BL Connecteur ganeshtdtactesbl : ');
-$requeteExtension = "SELECT id_e FROM extension WHERE path = ?";
-$ext_ganeshtdtactesbl = "/var/www/pastell/extensionbl/ganeshtdtactesbl/";
-if (!$sqlQuery->queryOne($requeteExtension, $ext_ganeshtdtactesbl)) {
-    $sqlQuery->queryOne("INSERT INTO extension (path) VALUES(?)", $ext_ganeshtdtactesbl);
-    $blScript->traceln('OK');
-} else {
-    $blScript->traceln('DEJA FAIT');
-}
+$blScript->traceln('Nettoyage des enregistrements faisant référence à des documents inexistants : ');
+// Table document_action_entite
+$sql = 'DELETE document_action_entite.* FROM document_action_entite';
+$sql .= ' INNER JOIN document_action ON document_action_entite.id_a=document_action.id_a';
+$sql .= ' LEFT JOIN document ON document_action.id_d = document.id_d';
+$sql .= ' WHERE document.id_d is null';
+$rowCount = $sqlQuery->getPdo()->exec($sql);
+$blScript->traceln('    document_action_entite : ' . $rowCount . ' suppressions.');
+
+// Table document_action
+$sql = 'DELETE document_action.* FROM document_action';
+$sql .= ' LEFT JOIN document ON document_action.id_d = document.id_d';
+$sql .= ' WHERE document.id_d is null';
+$rowCount = $sqlQuery->getPdo()->exec($sql);
+$blScript->traceln('    document_action : ' . $rowCount . ' suppressions.');
+
