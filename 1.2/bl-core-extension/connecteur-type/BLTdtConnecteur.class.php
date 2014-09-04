781f0216cecca7bce7c2a3915514489f09ba24be
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2014-09-04 15:39:53 +0000

    m45270 : ajout de la taille du dossier complet + généralisation à tous les types de document 	- nouvel attribut dans le journal : tailletot, pour la taille du dossier complet. L'attribut taille mentionne la taille du document principal. 	- les informations apparaissent aussi dans le texte du message, affiché par l'ihm et dans les logs d'actions automatiques.

--- 1.2/bl-core-extension/connecteur-type/BLTdtConnecteur.class.php
+++ 1.2/bl-core-extension/connecteur-type/BLTdtConnecteur.class.php
@@ -8,6 +8,10 @@ require_once(PASTELL_PATH . "/connecteur-type/TdtConnecteur.class.php");
 abstract class BLTdtConnecteur extends TdtConnecteur {
 
     const KEY_INFO_FILESIZE = 'filesize';
+    const ATTR_FICHIER_PRINCIPAL = 'document_principal';
+    const ATTR_VISUEL_PDF = 'visuel_pdf';
+    const ATTR_PJ = 'piece_jointe';
+
     // Attributs de flux, spécifiques à ce type de connecteur;
     // accessibles uniquement par les connecteurs de ce type.
     const ATTR_TDT_TRANSACTION_ID = 'tdt_transaction_id';
