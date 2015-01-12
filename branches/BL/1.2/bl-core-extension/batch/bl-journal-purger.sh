23ba7274be6bdd28d1f432bfa639f4b196eb73de
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-01-12 10:35:51 +0000

    - m48749 : purger le journal (suite)     . homogénisation des logs     . bloquer en cas de maintenance applicative     . l'erreur "No entry for terminal type "unknown"" s'affichait toujours

--- branches/BL/1.2/bl-core-extension/batch/bl-journal-purger.sh
+++ branches/BL/1.2/bl-core-extension/batch/bl-journal-purger.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+# ATTENTION : utiliser les sauts de ligne LINUX et pas WINDOWS
+
+echo [$(date +%d-%m-%Y\ %H\:%M\:%S)] Demarrage $0
 
 # éviter les erreurs "No entry for terminal type "unknown";" lors de l'exécution par cron
 if [ ! $TERM ]; then
     export TERM=xterm
 fi
 
-echo "[$(date +%d-%m-%Y\ %H\:%M\:%S)] $0 - démarrage"
+if [ ! -z $(find /tmp -maxdepth 1 -iname '*.lock' -print -quit) ]
+then
+    echo Application en cours de maintenance.
+else
+    sudo -H -u www-data /usr/bin/php /var/www/pastell/bl-core-extension/batch/bl-journal-purger.php nb_jours_conservation=60
+fi
 
-sudo -H -u www-data /usr/bin/php /var/www/pastell/bl-core-extension/batch/bl-journal-purger.php nb_jours_conservation=60
-
-echo "[$(date +%d-%m-%Y\ %H\:%M\:%S)] $0 - fin"
+echo [$(date +%d-%m-%Y\ %H\:%M\:%S)] Terminaison $0
 
