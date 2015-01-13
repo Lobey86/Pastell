a5b81c7a2f9641b04aebc2c39a3f88f7b7a056f7
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-01-13 17:03:17 +0000

    - 48364 : Connecteur SAE Imprimerie Nationale (version pour provisioning) (suite)     . homogénéisation et lisibilité des noms d'attributs

--- 1.2/bl-core-extension/connecteur-type/BLSaeConnecteur.class.php
+++ 1.2/bl-core-extension/connecteur-type/BLSaeConnecteur.class.php
@@ -7,6 +7,14 @@ require_once(PASTELL_PATH . "/pastell-core/Connecteur.class.php");
  */
 abstract class BLSaeConnecteur extends Connecteur {
 
+    // Attributs de service versant
+    const ATTR_SAE_VERSANT_DESCRIPTION = 'versant_description';
+    const ATTR_SAE_VERSANT_IDENTIFICATION = 'versant_identification';
+    const ATTR_SAE_VERSANT_CONTACT_SERVICE = 'versant_contact_service';
+    const ATTR_SAE_VERSANT_CONTACT_PERSONNE = 'versant_contact_personne';
+    const ATTR_SAE_VERSANT_CONTACT_FONCTION = 'versant_contact_fonction';
+    const ATTR_SAE_VERSANT_ADRESSE = 'versant_adresse';
+    
     // Attributs de flux, spécifiques à ce type de connecteur; 
     // accessibles uniquement par les connecteurs de ce type.
     //
@@ -16,9 +24,14 @@ abstract class BLSaeConnecteur extends Connecteur {
     const ATTR_SAE_AR_SEDA = 'ar_seda';
     const ATTR_SAE_AR_SEDA_COMMENTAIRE = 'ar_seda_commentaire';
 
+    // Etats de versement
+    const AR_ATTENTE = 'attente';
+    const AR_OK = 'ok';
+    const AR_REJET = 'rejet';
+    
     public abstract function testConnexion();
     public abstract function genererSedaHelios();
-    public abstract function deposerSedaHelios();
-    public abstract function verifierEtatDepotSedaHelios();
+    public abstract function verserSedaHelios();
+    public abstract function arSedaHelios();
     
 }
