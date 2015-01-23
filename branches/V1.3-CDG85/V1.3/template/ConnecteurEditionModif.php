1bfda604bf63eb3f67d7d10c1322bbb8ca41a909
arnauz <arnauz@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-01-23 10:31:34 +0000

    Suppression de la branche V1.3-CDG85

--- branches/V1.3-CDG85/V1.3/template/ConnecteurEditionModif.php
+++
@@ -1,7 +0,0 @@
-<a class='btn btn-mini' href='connecteur/edition.php?id_ce=<?php echo $id_ce?>'><i class='icon-circle-arrow-left'></i>Revenir à <?php echo $connecteur_entite_info['libelle']?></a>
-
-<div class="box">
-<h2>Connecteur <?php hecho($connecteur_entite_info['type']) ?> - <?php hecho($connecteur_entite_info['id_connecteur'])?> : <?php hecho($connecteur_entite_info['libelle']) ?> 
-</h2>
-
-<?php $this->render("DonneesFormulaireEdition"); ?></div>
