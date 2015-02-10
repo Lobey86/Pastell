eef84fb8c24dcec9ee38de105ea84b121b29b046
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-02-10 14:56:18 +0000

    Mise à jour de jQuery en version 1.11.2

--- trunk/test/test.php
+++ trunk/test/test.php
@@ -1,44 +1,18 @@
 <html>
 <head>
 <script type="text/javascript" src="../web/js/jquery-1.11.2.min.js"></script>
-<script type="text/javascript" src='../web/js/jquery-ui.min.js'></script> 
+<script type="text/javascript" src='../web/js/jquery-ui-1.11.2.min.js'></script> 
      
-<script type="text/javascript" src="../web/js/htmlentities.js"></script>
+<script type="text/javascript" src="../web/js/pastell.js"></script>
+
+<link rel="stylesheet" href="../web/img/jquery-ui.theme.1.11.2.min.css">
 </head>
 <body>
 <input id="to" autocomplete='off' name="to" value="" size="40" type="text">
 <script>
 
-
-
 $(document).ready(function(){
-	$("#to").autocomplete({
-		 source: function( request, response ) {
-             $.ajax({
-                 url:  "../web/mailsec/get-contact-ajax.php",
-                 data: {q: request.term,id_e: 1},
-                 success: function(data) {
-                             /*response($.map(data, function(item) {
-                             return {
-                                 label: item.state,
-                                 id: item.id,
-                                 abbrev: item.abbrev
-                                 };
-                         }));*/
-                         var result = data.split("\n");
-                         console.log(result);
-                         response(result);
-                         
-                     
-                     }
-                 })},
-		multiple: true,
-		cacheLength:0,
-		max: 20
-
-	
-	
-	});
+	$("#to").pastellAutocomplete("../web/mailsec/get-contact-ajax.php");
 });
 </script>
 </body>
--- trunk/web/js/pastell.js
+++ trunk/web/js/pastell.js
@@ -18,3 +18,30 @@ $(document).ready(function() {
 	
 });
 
+function split( val ) {
+	return val.split( /,\s*/ );
+}
+
+function extractLast( term ) {
+	return split( term ).pop();
+}
+
+$.fn.pastellAutocomplete = function(autocomplete_url,id_e,mail_only) {
+	this.autocomplete({
+		source: function( request, response ) {
+			$.getJSON( autocomplete_url, {
+				term: extractLast( request.term ), "id_e": id_e, "mail-only": mail_only
+			}, response ); 
+		},
+		select: function( event, ui ) {
+			var terms = split( this.value );
+			terms.pop();
+			terms.push( ui.item.value );
+			terms.push( "" );
+			this.value = terms.join( ", " );
+			return false;
+		} ,
+	});
+	return this;
+}
+
