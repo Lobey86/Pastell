3f51d196bbd96eca1d9ffa706a8970106ec87d59
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-02-11 08:02:18 +0000

    Traitement par lot : Les actions affiché sont l'intersection des actions possibles. L'affichage est automatique sans rechargement (jquery)

--- template/DocumentTraitementLot.php
+++ template/DocumentTraitementLot.php
@@ -19,7 +19,7 @@
 			<?php foreach($listDocument as $i => $document ) : ?>
 			<tr>
 				<td>
-					<input type='checkbox' name='id_d[]' value='<?php echo $document['id_d']?>'/>
+					<input class='document_checkbox' type='checkbox' name='id_d[]' value='<?php echo $document['id_d']?>'/>
 				</td>
 				<td>
 				<a href='document/detail.php?id_d=<?php echo $document['id_d']?>&id_e=<?php echo $document['id_e']?>'>
@@ -43,7 +43,79 @@
 		<?php endforeach;?>
 		</table>
 		<?php foreach($all_action as $action_name):?>
-			<button type='submit' class='btn' name='action' value='<?php echo $action_name?>'><?php hecho($theAction->getDoActionName($action_name)) ?></button>&nbsp;&nbsp;
+			<span class='action_submit' id='btn_<?php echo $action_name?>'>
+			<button type='submit'  class='btn ' name='action' value='<?php echo $action_name?>'><?php hecho($theAction->getDoActionName($action_name)) ?></button>
+			&nbsp;&nbsp;
+			</span>
 		<?php endforeach;?>
+		<br/>
+		<div class='alert alert_info' id='btn_message'></div>
 	</form>
 </div>
+<script>
+
+var all_tab = {
+	<?php foreach($listDocument as $i => $document ) : ?>
+		'<?php echo $document['id_d']?>': [
+			<?php foreach($document['action_possible'] as $action_name) : ?>
+			'<?php echo $action_name ?>',
+			<?php endforeach;?>
+		
+		],	
+	<?php endforeach;?>
+};
+
+function array_intersection(array1,array2){
+	return array1.filter(function(n) {
+	    return array2.indexOf(n) != -1
+	});
+
+}
+
+$(document).ready(function(){
+
+	$(".action_submit").hide();
+	$("#btn_message").html("Veuillez sélectionner un ou plusieurs documents");
+	
+	$(".document_checkbox").click(function(){
+
+		var checkedValues = $('.document_checkbox:checked').map(function() {
+		    return this.value;
+		}).get();
+		var tab_result = [];
+		for(i=0; i<checkedValues.length; i++){
+			var id_d = checkedValues[i];
+			var tab_tmp = all_tab[id_d];
+			if (i == 0){
+				tab_result = tab_tmp;
+			} else {
+				tab_result = array_intersection(tab_result,tab_tmp);
+			}
+			
+		}
+		
+		$(".action_submit").each(function(){
+			var action_id = this.id.substr(4);
+			if ( tab_result.indexOf(action_id) == -1){
+				$(this).hide();
+			} else {
+				$(this).show();
+				$("#btn_message").hide();
+			}
+		});
+
+		if (tab_result.length==0){
+			$("#btn_message").show();
+			if (checkedValues.length > 0){
+				$("#btn_message").html("Aucune action n'est commune aux documents sélectionnés");
+			} else {
+				$("#btn_message").html("Veuillez sélectionner un ou plusieurs documents");
+			}
+		}
+		
+		
+	});
+});
+
+
+</script>
