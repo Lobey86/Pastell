ecc1d1ccf5ec862639fee4361fc1a6fe0025542c
pbonifacj <pbonifacj@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-02-12 18:31:19 +0000

    - m49739: Conserver en ligne les versions des sources des scripts de mise à jour     . le n° du premier script à jouer est mentionné lors du build     . ce n° est stocké dans un fichier de config, situé au même endroit que les scripts de mise à jour     . le script d'enchaînement des mises à jour parcourt les scripts de mise à jour, par ordre alpha-numérique, et n'exécute que ceux dont le nom est supérieur ou égal à celui configuré.     . les todolist de chaque script sont regroupées, mémorisées dans un fichier, et affichées en fin d'enchaînement

--- branches/BL/1.2/bl-core-extension/installation/busbl_updatedata.sh
+++ branches/BL/1.2/bl-core-extension/installation/busbl_updatedata.sh
@@ -1,17 +1,34 @@
 #!/bin/sh
 
 # Exécution des scripts qui se trouvent dans le répertoire du script courant.
-# Il faut mutliplier les scripts si entre 2 scripts le contexte ou du cache doivent être réinitialisés.
-# Les scripts exécutés doivent se nommer : BLScriptUpdateVersion_XX.php (ex: BLScriptUpdateVersion_01.php, BLScriptUpdateVersion_02.php)
-# Ils sont exécutés dans l'ordre de leur numéro
+# Le premier des scripts à exécuté est mentionné dans le fichier de configuration. Si non défini, tous sont exécutés.
+# Les scripts exécutés doivent se nommer : BLScriptUpdateVersion_XX.php (ex: BLScriptUpdateVersion_01.php, BLScriptUpdateVersion_02.php, BLScriptUpdateVersion_01.1.php)
+# Ils sont exécutés dans l'ordre alpha-numérique.
 # S'il existe qu'un seul script, il doit quand-même être numéroté (ex: BLScriptUpdateVersion_01.php)
+# Il faut multiplier les scripts si entre 2 scripts le contexte ou du cache doivent être réinitialisés.
 
 echo "====> Montée de version des données..."
 
-for i in $(find $(dirname $0)/ -name BLScriptUpdateVersion*.php -type f | sort) ;
+. $(dirname $0)/busbl_updatedata.cfg
+
+TODOLIST_FILEPATH="/tmp/todolist.txt"
+
+if [ -f $TODOLIST_FILEPATH ]; then
+    rm $TODOLIST_FILEPATH;
+fi
+
+for script in $(find $(dirname $0)/ -name "BLScriptUpdateVersion*.php" -type f | sort) ;
 do
-    if [ -f "$i" ]; then
-        echo "--> Execution du script : $i"
-        sudo -H -u www-data /usr/bin/php "$i"
+    if [ ! "$(basename $script)" \< "$script_premier" ]; then
+        echo "--> Exécution du script : $script"
+        sudo -H -u www-data /usr/bin/php "$script" todolist_filepath=$TODOLIST_FILEPATH
     fi
 done
+
+if [ -f $TODOLIST_FILEPATH ]; then
+    echo ""
+    echo "====> Paramétrages complémentaires à effectuer :"
+    cat $TODOLIST_FILEPATH
+    echo ""
+fi
+
