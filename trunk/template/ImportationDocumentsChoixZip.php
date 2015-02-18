191efd8d3b1fc3916408907f49ca072af4be57c3
iruiz <iruiz@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-02-18 16:09:39 +0000

    Ajout du template ImportationDocumentsChoixZip.php pour l'extension ptl-mgl

---
+++ trunk/template/ImportationDocumentsChoixZip.php
@@ -0,0 +1,12 @@
+<form action='document/external-data-controler.php' method='post'>
+	<input type='hidden' name='id_d' value='<?php echo $id_d?>' />
+	<input type='hidden' name='id_e' value='<?php echo $id_e?>' />
+	<input type='hidden' name='page' value='<?php echo $page?>' />
+	<input type='hidden' name='field' value='<?php echo $field?>' />
+	<select name='choix_zip'>
+	<?php foreach($file_zip as $num => $nom_element) : ?>
+		<option value='<?php hecho($nom_element) ?>'><?php hecho($nom_element)?></option>
+	<?php endforeach; ?>
+	</select>	
+	<input type='submit' class='btn' value='Sélectionner'/>
+</form>
