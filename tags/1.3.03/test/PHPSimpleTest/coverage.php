745c09499bae90b09ffd9bb1fcc627392657c465
arnauz <arnauz@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-03-05 09:36:45 +0000

    Suppression du tag 1.3.03

--- tags/1.3.03/test/PHPSimpleTest/coverage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("ZenTest.class.php");
-
-$zenTest = new ZenTest(dirname(__FILE__));
-$zenTest->addToIncludePath(dirname(__FILE__)."/../lib");
-
-$zenTest->coverage();
-header("location: report");
