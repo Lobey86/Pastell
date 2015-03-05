20e246ca3b64ac91de9483f2accf64a0d2b0b970
epommate <epommate@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-03-05 15:27:11 +0000

    [Pastell CORE] Ajout de la clé "visionneuse" dans les élements du formulaire permettant l'ajout d'un lien "voir" qui permet la visualisation d'un fichier métier via une classe de type visionneuse. [Helios generique] Ajout d'une visionneuse sur les fichier PES Acquit

--- template/DonneesFormulaireDetail.php
+++ template/DonneesFormulaireDetail.php
@@ -25,6 +25,37 @@
 							</a>
 						<?php endif;?>
 				<?php endforeach;?>
+				<?php if($displayField->getField()->getVisionneuse()):?>
+					<a class='visionneuse_link' href='document/visionneuse.php?id_e=<?php echo $id_e?>&id_d=<?php hecho($id_d)?>&field=<?php hecho($displayField->getField()->getName()) ?>'>Voir</a>
+					<div class='visionneuse_result'></div>
+					<script>
+$(document).ready(function(){
+
+	$(".visionneuse_result").hide();
+	
+	$('.visionneuse_link').click(function(){
+		var link=$(this).attr('href');
+		var visionneuse_result = $(this).next(".visionneuse_result");
+		visionneuse_result.toggle();
+		
+		//if (visionneuse_result.html() == ''){
+			$.ajax({
+				url: link,
+				cache: false
+			}).done(function( html ) {
+				visionneuse_result.html( html );
+			});
+		//};
+		
+		return false;
+	});
+
+
+	
+});
+					</script>
+					
+				<?php endif;?>
 			</td>
 		</tr>
 <?php endforeach;?>
