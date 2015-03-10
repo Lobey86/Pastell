a4bb2ffce42f4cded3c4c8d1e4a9f6cfc28ea874
arnauz <arnauz@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-03-10 08:20:17 +0000

    Suppression du tag 1.3.03

--- tags/V1.3.03/template/SirenBox.php
+++
@@ -1,7 +0,0 @@
-<?php if (! PRODUCTION) : ?>
-<div class="alert alert-info">
-<strong>Version de démonstration</strong>
-<br/>
-Exemple de siren valide : <?php hecho($this->Siren->generate()) ?>
-</div>
-<?php endif;?>
--- tags/V1.3.03/web/api/detail-several-document.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("init-api.php");
-
-$recuperateur = new Recuperateur($_REQUEST);
-$all_id_d = $recuperateur->get('id_d');
-$id_e = $recuperateur->get('id_e');
-
-$api_json->detailSeveralDocument($id_e, $all_id_d);
-
