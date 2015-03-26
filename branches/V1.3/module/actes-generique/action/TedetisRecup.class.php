4d72044f7690dfa1a453ca90160dcebf02deb9b6
iruiz <iruiz@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-03-26 09:59:17 +0000

    Branche 1.3, report de la rev 1263: [actes-generique] Le borderdereau et l'actes tamponné sont récupéré avant de passé la transaction au statut acquitté afin d'éviter de ne pas les récuprer en cas de problème réseau.

--- branches/V1.3/module/actes-generique/action/TedetisRecup.class.php
+++ branches/V1.3/module/actes-generique/action/TedetisRecup.class.php
@@ -42,6 +42,9 @@ class TedetisRecup extends ActionExecutor {
 		
 		$aractes = $tdT->getARActes();
 		
+		$bordereau_data = $tdT->getBordereau($tedetis_transaction_id);
+		$actes_tamponne = $tdT->getActeTamponne($tedetis_transaction_id);
+		
 		$actionCreator->addAction($this->id_e,0,'acquiter-tdt',"L'acte a été acquitté par le contrôle de légalité");
 		
 		$infoDocument = $this->getDocument()->getInfo($this->id_d);
@@ -49,11 +52,7 @@ class TedetisRecup extends ActionExecutor {
 		$infoUser = $documentActionEntite->getUserFromAction($this->id_e,$this->id_d,'send-tdt');
 		$message = "L'acte « {$infoDocument['titre']} » télétransmis par {$infoUser['prenom']} {$infoUser['nom']} a été acquitté par le contrôle de légalité";
 		
-		$message .= "\n\nConsulter le détail de l'acte : " . SITE_BASE . "document/detail.php?id_d={$this->id_d}&id_e={$this->id_e}";
-		
-		$bordereau_data = $tdT->getBordereau($tedetis_transaction_id);
-		$actes_tamponne = $tdT->getActeTamponne($tedetis_transaction_id);
-		
+		$message .= "\n\nConsulter le détail de l'acte : " . SITE_BASE . "document/detail.php?id_d={$this->id_d}&id_e={$this->id_e}";		
 		
 		$donneesFormulaire = $this->getDonneesFormulaire();
 		if ($bordereau_data){
