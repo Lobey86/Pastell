d527aea077a20222f5a35d6b4bf9faa6f3eb264c
iruiz <iruiz@9fa3cec2-9314-4d4e-a313-b2822cbb6d99>
2015-03-27 09:07:51 +0000

    Branche 1.3, report de la rev 1266:  [actes-generique] Lors du retour de la télétransmission (certificat RGS**), on vérifie sur S2low que l'acte est bien parti (i.e. il n'est pas dans l'état en attente d'être posté) avant de le faire avancé dans l'étape Pastell send-tdt.

--- branches/V1.3/module/actes-generique/action/TdtRetourTeletransmettre.class.php
+++ branches/V1.3/module/actes-generique/action/TdtRetourTeletransmettre.class.php
@@ -9,6 +9,17 @@ class TdtRetourTeletransmettre extends ActionExecutor {
 			throw new Exception("Erreur sur le Tdt : " . $message);
 		}
 		
+		$tdt = $this->getConnecteur("TdT");
+		
+		$tedetis_transaction_id = $this->getDonneesFormulaire()->get('tedetis_transaction_id');
+		
+		$status =  $tdt->getStatus($tedetis_transaction_id);
+		
+		//A priori, c'est le seul cas que je vois ou la transaction n'a pas encore été posté
+		if (in_array($status, array(TdtConnecteur::STATUS_ACTES_EN_ATTENTE_DE_POSTER))){
+			throw new Exception("La transaction n'a pas le bon status : ".TdtConnecteur::getStatusString($status)." trouvé" ) ;
+		}
+		
 		$this->changeAction("send-tdt", "Le document à été télétransmis à la préfecture");
 		$this->notify('send-tdt', $this->type,"Le document à été télétransmis à la préfecture");
 		
